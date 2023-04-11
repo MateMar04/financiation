f20268ef66a196e25e86530992d963de014ce034
martiarena842 <tomimartiarena@gmail.com>
2023-04-11 11:36:22 -0300

    new urls

--- financiationDjango/djangoapp/serializers.py
+++ financiationDjango/djangoapp/serializers.py
@@ -5,4 +5,4 @@ class ProjectSerializer(serializers.ModelSerializer) :
     class Meta:
         model = Project
         fields = ('id', 'title', 'description', 'technology', 'created_at')
-        read_only_fields = ('created_at')
+        read_only_fields = ('created_at', )
