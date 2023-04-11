e076578de71d850fe036ff2e5fc5bd34578bf8a3
LautaroVelez <lautivelez28@gmail.com>
2023-04-11 12:01:58 -0300

    branch creada

---
+++ financiation-react/src/components/login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+
+function Login() {
+    return(
+        <div>
+        <h1>LOG IN</h1>
+        <button type="button" class="btn btn-outline-dark">Log in</button>
+        </div>
+        );
+}
+
+export default Login;
