c4343bf860248c5c6df9b908a2dc35280c31d821
mateo <mateo.marchisone@gmail.com>
2023-04-13 16:48:53 -0300

    Log-In and Sing In

--- financiation-react/src/components/bienvenida.js
+++ financiation-react/src/components/bienvenida.js
@@ -15,14 +15,15 @@ function Bienvenida() {
             <div className="container">
               <img src={LOGOGOBIERNO} alt="React Logo" className="img-fluid"/>
             </div>
-            <div>
+            <hr/>
+            <div className="contenedor">
               <p>Si todavia no tenes una cuenta puede crearte una</p>
-              <input type="button" value="Sign-In" className="boton"/>
+              <input type="button" value="Crear" className="boton"/>
             </div>
             <hr/>
-            <div>
+            <div className="contenedor">
               <p>Si ya tienes una cuenta accede</p>
-              <input type="button" value="Log-In" className="boton"/>
+              <input type="button" value="Acceder" className="boton"/>
             </div>
           </div>
         </div>
