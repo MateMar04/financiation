51132c93bb8f25c83f29da6133b6493ee783e98a
mateosinches <71389954+mateosinches@users.noreply.github.com>
2023-04-13 09:33:28 -0300

    avance en js nav

---
+++ financiation-react/src/Components/Navbar.js
@@ -0,0 +1,82 @@
+import React from "react"
+import "../Assets/Styles/Navbar.css"
+
+function Navbar() {
+    return (
+        <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+
+            <div class="container">
+
+                <a class="navbar-brand" href="#"><i class="fab fa-linkedin fa-2x"></i></a>
+                <form class="input-group" style="width: 400px">
+                    <input type="search" class="form-control" placeholder="Type query" aria-label="Search" />
+                    <button class="btn btn-outline-primary" type="button" data-mdb-ripple-color="dark" style="padding: .45rem 1.5rem .35rem;">
+                        Search
+                    </button>
+                </form>
+
+                <button class="navbar-toggler" type="button" data-mdb-toggle="collapse"
+                    data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <i class="fas fa-bars"></i>
+                </button>
+
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active d-flex flex-column text-center" aria-current="page" href="#"><i class="fas fa-home fa-lg"></i><span class="small">Home</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link d-flex flex-column text-center" aria-current="page" href="#"><i class="fas fa-user-friends fa-lg"></i><span class="small">My Network</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link d-flex flex-column text-center" aria-current="page" href="#"><i class="fas fa-briefcase fa-lg"></i><span class="small">Jobs</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link d-flex flex-column text-center" aria-current="page" href="#"><i class="fas fa-comment-dots fa-lg"></i><span class="small">Messaging</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link d-flex flex-column text-center" aria-current="page" href="#"><i class="fas fa-bell fa-lg"></i><span class="small">Notifications</span></a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a
+                                class="nav-link dropdown-toggle d-flex align-items-center"
+                                href="#"
+                                id="navbarDropdownMenuLink"
+                                role="button"
+                                data-mdb-toggle="dropdown"
+                                aria-expanded="false"
+                            >
+                                <img
+                                    src="https://mdbootstrap.com/img/Photos/Avatars/img%20(9).jpg"
+                                    class="rounded-circle"
+                                    height="30"
+                                    alt=""
+                                    loading="lazy"
+                                />
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <li><a class="dropdown-item" href="#">My profile</a></li>
+                                <li><a class="dropdown-item" href="#">Settings</a></li>
+                                <li><a class="dropdown-item" href="#">Logout</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+
+                </div>
+
+            </div>
+
+        </nav>
+        
+
+    
+
+
+    );
+}
+
+
+export default Navbar;
