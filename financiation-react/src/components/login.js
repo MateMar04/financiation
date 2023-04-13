34dc73ddce7a9cb343ec3be7addde19ac543b2b1
LautaroVelez <lautivelez28@gmail.com>
2023-04-13 09:45:47 -0300

    actualizacion

--- financiation-react/src/components/login.js
+++ financiation-react/src/components/login.js
@@ -2,23 +2,24 @@ import React from 'react';
 import '../assets/styles/login.css'
 
 
+
 function Login() {
     return (
         <div>
             <div class="wrapper fadeInDown">
                 <div id="formContent">
                     <div class="fadeIn first">
-                        <img id="icon" alt="User Icon" />
+                        <img id="Logo" src=''/>
                     </div>
 
-                    <input type="text" id="login" class="fadeIn second" name="login" placeholder="login"/>
-                        <input type="text" id="password" class="fadeIn third" name="login" placeholder="password"/>
-                            <input type="submit" class="fadeIn fourth" value="Log In"/>
-
-
+                    <input type="text" id="CUIL" class="fadeIn second" name="User" placeholder="Usuario"/>
+                        <input type="password" id="Contraseña" class="fadeIn third" name="Contraseña" placeholder="Contraseña"/>
+                            <input type="submit" class="fadeIn fourth" value="Entrar"/>
+                            <a class="register" href="#">Registrarse</a>
+                          
 
                                 <div id="formFooter">
-                                    <a class="underlineHover" href="#">Forgot Password?</a>
+                                    <a class="underlineHover" href="#">Olvidó la contraseña?</a>
                                 </div>
                 </div>
             </div>
