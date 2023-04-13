da4d76863a649b484ca958788775a9c4ebc3206d
LautaroVelez <lautivelez28@gmail.com>
2023-04-13 08:32:06 -0300

    login ra-raw

--- financiation-react/src/App.js
+++ financiation-react/src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import Login from './components/login';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 function App() {
   return (
     <div className="App">
-        <Login/>
-        </div>
+      <Login/>
+    </div>
   );
 }
 
--- financiation-react/src/components/login.js
+++ financiation-react/src/components/login.js
@@ -1,14 +1,33 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
+import '../assets/login.css'
 
 
 function Login() {
-    return(
+    return (
         <div>
-        <h1>LOG IN</h1>
-        <button type="button" class="btn btn-outline-dark">Log in</button>
+            <div class="wrapper fadeInDown">
+                <div id="formContent">
+                    <div class="fadeIn first">
+                        <img id="icon" alt="User Icon" />
+                    </div>
+
+                    <input type="text" id="login" class="fadeIn second" name="login" placeholder="login"/>
+                        <input type="text" id="password" class="fadeIn third" name="login" placeholder="password"/>
+                            <input type="submit" class="fadeIn fourth" value="Log In"/>
+
+
+
+                                <div id="formFooter">
+                                    <a class="underlineHover" href="#">Forgot Password?</a>
+                                </div>
+                </div>
+            </div>
         </div>
-        );
+    );
 }
 
 export default Login;
+
+
+
+
