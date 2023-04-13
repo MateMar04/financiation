8969ece1319f210f6f2732c367a31e0f7b8b59e4
tadeofunes <71388800+tadeofunes@users.noreply.github.com>
2023-04-13 11:55:28 -0300

    Campos personalizados

--- financiation-react/src/Components/signIn.js
+++ financiation-react/src/Components/signIn.js
@@ -4,34 +4,39 @@ import '../assets/styles/signin.css';
 import '../assets/images/paulolondra.jpg';
 function Signinform() {
     return (
-    <div>
-      <div className="wrapper fadeInDown">
-        <div id="formContent">
-         
-          <div className="fadeIn first">
-            <img src="../assets/images/paulolondra.jpg" id="icon" alt="User Icon" />
-          </div>
-  
-          
-          <form>
-            <input class="inputizquierda" type="text" id="login" className="fadeIn second" name="signin" placeholder="Usuario" />
-            <input class="inputizquierda" type="password" id="password" className="fadeIn third" name="login" placeholder="Contrasenia" />
-            <input class="inputizquierda" type="text" id="password" className="fadeIn third" name="login" placeholder="Nombre" />
-            <input class="inputizquierda" type="text" id="password"  name="login" placeholder="Apellido"/>
-            <input class="inputderecha" type="text" id="nombre" name="nombre"/>
-            <input class="inputizquierda" type="submit" className="fadeIn fourth" value="Sign In" />
-            
-          </form>
-          
-            
-         
-          <div id="formFooter">
-            <a className="underlineHover" href="#">Forgot Password?</a>
-          </div>
+        <div>
+            <div className="wrapper fadeInDown">
+                <div id="formContent">
+                    <div className="fadeIn first">
+                        <img src="../assets/images/paulolondra.jpg" id="icon" alt="User Icon" />
+                    </div>
+
+                    <form>
+                        <div class='container-fluid'>
+                            <div class='row'>
+                                <div class='col-lg-6'>
+                                    <input type='text' placeholder='nombre' />
+                                    <input type='text' placeholder='mail' />
+                                </div>
+                                <div class='col-lg-6'>
+                                    <input type='text' placeholder='apellido' />
+                                    <input type='password' placeholder='contrasenia' />
+                                </div>
+                            </div>
+                        </div>
+                        <input class="inputizquierda" type="submit" className="fadeIn fourth" value="Sign In" />
+
+                    </form>
+
+
+
+                    <div id="formFooter">
+                        <a className="underlineHover" href="#">Forgot Password?</a>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
     );
-  }
-  
-  export default Signinform;
+}
+
+export default Signinform;
