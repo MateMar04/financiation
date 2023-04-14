79bf2c21bd26f326ebbaed6c2023ac75cc0b3f25
tadeofunes <71388800+tadeofunes@users.noreply.github.com>
2023-04-14 11:15:05 -0300

    Updateamos algunos campos

--- financiation-react/src/Components/signIn.js
+++ financiation-react/src/Components/signIn.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../assets/styles/signin.css';
-import '../assets/images/paulolondra.jpg';
+import '../assets/images/PRUEBA.PNG';
+let Logo = require('../assets/images/PRUEBA.PNG');
 function Signinform() {
     return (
         <div>
             <div className="wrapper fadeInDown">
                 <div id="formContent">
                     <div className="fadeIn first">
-                        <img src="../assets/images/paulolondra.jpg" id="icon" alt="User Icon" />
+                       <img src={Logo} alt="React Logo" id="LOGOGOB" />
                     </div>
 
                     <form>
                         <div class='container-fluid'>
                             <div class='row'>
                                 <div class='col-lg-6'>
-                                    <input type='text' placeholder='nombre' />
-                                    <input type='text' placeholder='mail' />
+                                    <input type='text' placeholder='Usuario' />
+                                    <input type='text' placeholder='Contraseña' />
+                                    <input type='text' placeholder='Nombre' />
                                 </div>
                                 <div class='col-lg-6'>
-                                    <input type='text' placeholder='apellido' />
-                                    <input type='password' placeholder='contrasenia' />
+                                    <input type='email' placeholder='Mail' id='botonemail' />
+                                    <input type='password' placeholder='Repetir Contraseña' />
+                                    <input type='text' placeholder='Apellido' />
                                 </div>
                             </div>
                         </div>
