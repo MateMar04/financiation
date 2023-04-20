98678d48fc1f30f2f7e89bc7d54eb7a44235d6b3
mateosinches <71389954+mateosinches@users.noreply.github.com>
2023-04-20 11:01:52 -0300

    aa

--- financiation-react/src/Components/Navbar.js
+++ financiation-react/src/Components/Navbar.js
@@ -1,81 +1,58 @@
 import React from "react"
-import "../Assets/Styles/Navbar.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import "../Assets/Styles/Navbar.css"
+let Logo = require('../Assets/Imagenes/LOGOGOBIERNO.png');
 
-function Navbar() {
+function NavScrollExample() {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-
-            <div class="container">
-
-                <a class="navbar-brand" href="#"><i class="fab fa-linkedin fa-2x"></i></a>
-                <form class="input-group">
-                    <input type="search" class="form-control" placeholder="¿Que estas buscando?" aria-label="Search" />
-                    <button class="btn btn-outline-primary" type="button" data-mdb-ripple-color="dark" >
-                        Buscar
-                    </button>
-                </form>
-
-                <button class="navbar-toggler" type="button" data-mdb-toggle="collapse"
-                    data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <i class="fas fa-bars"></i>
-                </button>
-
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active d-flex flex-column text-center" aria-current="page" href="#"><i class="fas fa-home fa-lg"></i><span class="small">Home</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link d-flex flex-column text-center" aria-current="page" href="#"><i class="fas fa-user-friends fa-lg"></i><span class="small">Contacto</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link d-flex flex-column text-center" aria-current="page" href="#"><i class="fas fa-briefcase fa-lg"></i><span class="small">Nosotro</span></a>
-                        </li>
-                        
-                        <li class="nav-item">
-                            <a class="nav-link d-flex flex-column text-center" aria-current="page" href="#"><i class="fas fa-bell fa-lg"></i><span class="small">Notificaciones</span></a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a
-                                class="nav-link dropdown-toggle d-flex align-items-center"
-                                href="#"
-                                id="navbarDropdownMenuLink"
-                                role="button"
-                                data-mdb-toggle="dropdown"
-                                aria-expanded="false"
-                            >
-                                <img
-                                    src="https://mdbootstrap.com/img/Photos/Avatars/img%20(9).jpg"
-                                    class="rounded-circle"
-                                    height="30"
-                                    alt=""
-                                    loading="lazy"
-                                />
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                <li><a class="dropdown-item" href="#">My profile</a></li>
-                                <li><a class="dropdown-item" href="#">Settings</a></li>
-                                <li><a class="dropdown-item" href="#">Logout</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-
+        <Navbar expand="lg" id="navbarcs">
+            <Container fluid>
+                <div id="container">
+                    <img className="img-fluid" src={Logo} alt="React Logo" />
                 </div>
-
-            </div>
-
-        </nav>
-
-
-
-
-
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                        className="me-auto my-2 my-lg-0"
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                    >
+                        <Nav.Link href="#action1" id="homecs">Home</Nav.Link>
+                        <Nav.Link href="#action2" id="contactocs">Contacto</Nav.Link>
+                        <NavDropdown title="Nosotros" id="navbarScrollingDropdown">
+                            <NavDropdown.Item href="#action3" >Nosotros</NavDropdown.Item>
+                            <NavDropdown.Item href="#action4">
+                                Another action
+                            </NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action5">
+                                Something else here
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link href="#" id="notics">
+                            Notificaciones
+                        </Nav.Link>
+                    </Nav>
+                    <Form className="d-flex">
+                        <Form.Control
+                            type="search"
+                            placeholder="¿Que estas buscando?"
+                            className="me-2"
+                            aria-label="Search"
+                            id="inputcs"
+                        />
+                        <Button variant="outline-success" id="buttoncs">Buscar</Button>
+                    </Form>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
 
-
-export default Navbar;
+export default NavScrollExample;
