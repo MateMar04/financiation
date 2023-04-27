049cb7bc60dfd918441ec6c3f9310822177702ff
Mateo Marchisone <mateo.marchisone@gmail.com>
2023-04-26 21:47:46 -0300

    Routes

--- financiation-react/src/App.js
+++ financiation-react/src/App.js
@@ -1,16 +1,36 @@
 import './App.css';
-import NavScrollExample from "./components/Navbar";
-import Bienvenida from "./components/Bienvenida";
-import LogIn from "./components/LogIn";
-import SignIn from "./components/SignIn";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Landing from "./containers/Landing";
+import Activate from "./containers/Activate";
+import LogIn from "./containers/LogIn"
+import SignUp from "./containers/SignUp";
+import ResetPassword from "./containers/ResetPassword";
+import ResetPasswordConfirm from "./containers/ResetPasswordConfirm";
+import Layout from "./hocs/Layout";
 
-function App() {
+const App = () => (
+    <Router>
+        <Layout>
+            <Routes>
+                <Route exact path='/' component={Landing}/>
+                <Route exact path='/login' component={LogIn}/>
+                <Route exact path='/signup' component={SignUp}/>
+                <Route exact path='/reset_password' component={ResetPassword}/>
+                <Route exact path='/password/reset/confirm/:uid/:token' component={ResetPasswordConfirm}/>
+                <Route exact path='/activate/:uid/:token' component={Activate}/>
+            </Routes>
+        </Layout>
+    </Router>
+)
+
+export default App;
+
+/*function App() {
   return (
     <div className="App">
         <Bienvenida/>
     </div>
   );
-}
+}*/
 
-export default App;
