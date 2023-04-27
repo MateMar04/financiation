b3903391639fede09e4bfaf9e2d2fffc4897313f
Mateo Marchisone <mateo.marchisone@gmail.com>
2023-04-26 22:03:52 -0300

    fixed routes

--- financiation-react/src/App.js
+++ financiation-react/src/App.js
@@ -13,12 +13,12 @@ const App = () => (
     <Router>
         <Layout>
             <Routes>
-                <Route exact path='/' component={Landing}/>
-                <Route exact path='/login' component={LogIn}/>
-                <Route exact path='/signup' component={SignUp}/>
-                <Route exact path='/reset_password' component={ResetPassword}/>
-                <Route exact path='/password/reset/confirm/:uid/:token' component={ResetPasswordConfirm}/>
-                <Route exact path='/activate/:uid/:token' component={Activate}/>
+                <Route exact path='/' element={<Landing/>}/>
+                <Route path='/login' element={<LogIn/>}/>
+                <Route exact path='/signup' element={<SignUp/>}/>
+                <Route exact path='/reset-password' element={<ResetPassword/>}/>
+                <Route exact path='/password/reset/confirm/:uid/:token' element={<ResetPasswordConfirm/>}/>
+                <Route exact path='/activate/:uid/:token' element={<Activate/>}/>
             </Routes>
         </Layout>
     </Router>
--- financiation-react/src/containers/LogIn.js
+++ financiation-react/src/containers/LogIn.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const LogIn = () => (
     <div>
-        LogIn
+        Login
     </div>
 );
 
