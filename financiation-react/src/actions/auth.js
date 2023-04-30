1c6a4c3b79066670395e89fc6e4ec5423c24e10e
Mateo Marchisone <mateo.marchisone@gmail.com>
2023-04-30 12:54:57 -0300

    checkAuthenticated()

--- financiation-react/src/actions/auth.js
+++ financiation-react/src/actions/auth.js
@@ -1,7 +1,51 @@
 import axios from "axios";
-import {LOGIN_FAIL, LOGIN_SUCCESS, USER_LOADED_FAIL, USER_LOADED_SUCCESS, AUTHENTICATED_SUCCESS, AUTHENTICATED_FAIL, LOGOUT} from "./types";
+import {
+    AUTHENTICATED_FAIL,
+    AUTHENTICATED_SUCCESS,
+    LOGIN_FAIL,
+    LOGIN_SUCCESS,
+    LOGOUT,
+    USER_LOADED_FAIL,
+    USER_LOADED_SUCCESS
+} from "./types";
 
+export const checkAuthenticated = () => async dispatch => {
+    if (localStorage.getItem('access')) {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        };
 
+        const body = JSON.stringify({token: localStorage.getItem('access')});
+
+        try {
+
+            const res = await axios.post(`${process.env.REACT_APP_API_URL}/auth/jwt/verify`, body, config);
+
+            if (res.data.code !== 'token_not_valid') {
+                dispatch({
+                    type: AUTHENTICATED_SUCCESS
+                });
+            } else {
+                dispatch({
+                    type: AUTHENTICATED_FAIL
+                });
+            }
+
+        } catch (err) {
+            dispatch({
+                type: AUTHENTICATED_FAIL
+            });
+        }
+
+    } else {
+        dispatch({
+            type: AUTHENTICATED_FAIL
+        });
+    }
+}
 
 export const load_user = () => async dispatch => {
     if (localStorage.getItem('access')) {
