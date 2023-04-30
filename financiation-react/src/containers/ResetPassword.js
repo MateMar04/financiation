0934c923fb3273e8a263f777d7706aed0e194730
Mateo Marchisone <mateo.marchisone@gmail.com>
2023-04-30 15:43:09 -0300

    ResetPassword.js

--- financiation-react/src/containers/ResetPassword.js
+++ financiation-react/src/containers/ResetPassword.js
@@ -1,9 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
+import {resetPassword} from "../actions/auth";
+import {Navigate} from "react-router-dom";
+import {connect} from "react-redux";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
-const ResetPassword = () => (
-    <div>
-        ResetPassword
-    </div>
-);
+const ResetPassword = (resetPassword) => {
 
-export default ResetPassword;
+    const [requestSent, setRequestSent] = useState(false);
+
+    const [formData, setFormData] = useState({
+        email: ''
+    });
+
+    const {email} = formData
+
+    const onChange = e => setFormData({...formData, [e.target.name]: e.target.value});
+
+    const onSubmit = e => {
+        e.preventDefault();
+
+        resetPassword(email)
+        setRequestSent(true)
+    };
+
+    if (requestSent) {
+        return <Navigate to='/'/>
+    }
+
+    return (
+        <div>
+            <Container fluid>
+                <h1>Reset Password</h1>
+                <Form onSubmit={e => onSubmit(e)}>
+                    <Form.Control type='email' placeholder='email' name='email' value='email'
+                                  onChange={e => onChange(e)} required></Form.Control>
+
+                    <Button type='submit'>Send Email</Button>
+                </Form>
+            </Container>
+        </div>
+    );
+};
+
+export default connect(null, {resetPassword})(ResetPassword);
