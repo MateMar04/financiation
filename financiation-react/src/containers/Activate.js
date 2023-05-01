208422e9195336bef756a7f37ba22d80ef018806
Mateo Marchisone <mateo.marchisone@gmail.com>
2023-05-01 20:19:55 -0300

    Activate.js

--- financiation-react/src/containers/Activate.js
+++ financiation-react/src/containers/Activate.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import {connect} from "react-redux";
+import {verify} from "../actions/auth";
+import '../assets/styles/login.css'
+import {Navigate} from "react-router-dom";
 
-const Activate = () => (
-    <div>
-        Activate
-    </div>
-);
+const Activate = ({verify, match}) => {
 
-export default Activate;
+    const [verified, setVerified] = useState(false)
+    const verify_account = e => {
+        const uid = match.params.uid
+        const token = match.params.token
+        verify(uid, token);
+        setVerified(true)
+    };
+
+    if (verified) {
+        return <Navigate to='/'/>
+    }
+
+    return (
+        <Container fluid>
+            <h1>Verify your account</h1>
+            <Button onClick={verify_account}>Verify</Button>
+        </Container>
+    )
+};
+
+
+export default connect(null, {verify})(Activate);
