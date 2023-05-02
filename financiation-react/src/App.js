4d8bb803f96f791f1c22b086ceaac550e8852fdf
tadeofunes <tade6funes@gmail.com>
2023-05-02 10:00:39 -0300

    finshed

--- financiation-react/src/App.js
+++ financiation-react/src/App.js
@@ -1,4 +1,23 @@
 import './App.css';
+import NavScrollExample from "./components/Navbar";
+import Bienvenida from "./components/bienvenida";
+import Login from "./components/login";
+import Signinform from "./components/signInForm";
+import MyModal from './components/succesfull';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+        <NavScrollExample />
+        <Bienvenida/>
+        <Login/>
+        <Signinform/>
+        <MyModal/>
+    </div>
+  );
+}
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Landing from "./containers/Landing";
@@ -11,15 +30,30 @@ import Layout from "./hocs/Layout";
 import {Provider} from "react-redux";
 import store from "./store";
 
-function App() {
+const App = () => (
+    <Provider store={store}>
+        <Router>
+            <Layout>
+                <Routes>
+                    <Route exact path='/' element={<Landing/>}/>
+                    <Route path='/login' element={<LogIn/>}/>
+                    <Route exact path='/signup' element={<SignUp/>}/>
+                    <Route exact path='/reset-password' element={<ResetPassword/>}/>
+                    <Route exact path='/password/reset/confirm/:uid/:token' element={<ResetPasswordConfirm/>}/>
+                    <Route exact path='/activate/:uid/:token' element={<Activate/>}/>
+                </Routes>
+            </Layout>
+        </Router>
+    </Provider>
+);
+
+export default App;
+
+/*function App() {
   return (
     <div className="App">
         <Bienvenida/>
-        <MyModal/>
     </div>
   );
 }*/
-
-
-export default App;
 
