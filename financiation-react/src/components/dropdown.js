87e15fec13ec61c34de9bd3f0777354b665abce5
LautaroVelez <lautivelez28@gmail.com>
2023-05-01 22:38:55 -0300

    checkpoint

--- financiation-react/src/components/dropdown.js
+++ financiation-react/src/components/dropdown.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import { Dropdown } from 'primereact/dropdown';
+import "../assets/styles/dropdown.css"
+import { Container } from 'react-bootstrap';
 
 export default function SelectArea() {
     const [selectedCity, setSelectedCity] = useState(null);
@@ -19,10 +21,12 @@ export default function SelectArea() {
     ];
 
     return (
+        <Container fluid className="">
         <div className="card flex justify-content-center">
             <Dropdown value={selectedCity} onChange={(e) => setSelectedCity(e.value)} options={cities} optionLabel="name"
                       placeholder="Seleccione Area" className="w-full md:w-14rem" />
         </div>
+        </Container>
     )
 }
 
--- financiation-react/src/components/formulario.js
+++ financiation-react/src/components/formulario.js
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
 import SelectArea from "./dropdown";
 import { Container } from 'react-bootstrap';
+import Logo from "../assets/images/PRUEBA.PNG";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "../assets/styles/formulario.css"
 
     function Formulario(){
         return(
-            <Container>
+            <Container fluid className="general">
+                <Container fluid className="image-container">
+                    <img src={Logo} alt="Logo del ministerio de finanzas"/>
+                </Container>
             <SelectArea/>
             </Container>
         );
