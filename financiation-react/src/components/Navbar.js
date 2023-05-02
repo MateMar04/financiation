251ec05080c0a99a487557d12909b6f352909b12
LautaroVelez <lautivelez28@gmail.com>
2023-05-01 23:59:31 -0300

    navbar finally fixed :)

--- financiation-react/src/components/Navbar.js
+++ financiation-react/src/components/Navbar.js
@@ -10,26 +10,28 @@ import logofinanzas from '../assets/images/logofinanzas.png';
 function NavScrollExample() {
     return (
         <Navbar expand="lg" id="navbarcs" className="navbarcs">
-            <Container fluid>
+            <Container className="col-md-2">
                 <Navbar.Brand href="#home" id="logoboton" alt='logoboton'><img src={logofinanzas} id='logoboton'
                     alt="logo de ministerio de finanzas"></img></Navbar.Brand>
-                <Navbar.Toggle aria-controls="navbarScroll" />
+            </Container>
 
+                <Navbar.Toggle aria-controls="navbarScroll"/>
                 <Navbar.Collapse>
-                    <Nav className="me-auto my-2 my-lg-0">
-
+                    <Container className="col-md-7">
+                    <Nav>
                         <Nav.Link href="#action1" id="navbar">Nueva Consulta</Nav.Link>
                         <Nav.Link href="#action2" id="navbar">Hoja de Rutas</Nav.Link>
                         <Nav.Link href="#action3" id="navbar">Grupos</Nav.Link>
                         <Nav.Link href="#action4" id="navbar">Calendario</Nav.Link>
                     </Nav>
+                    </Container>
 
-                    <Form className="d-flex">
+                    <Form className="accederbutton">
                         <Button variant="outline-light">Acceder</Button>{' '}
                     </Form>
 
                 </Navbar.Collapse>
-            </Container>
+
         </Navbar >
 
     );
