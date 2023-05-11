0898faf8a864a51224513b8fdd705136d2150321
tadeofunes <tade6funes@gmail.com>
2023-05-11 14:51:02 -0300

    Diferente contenedor

--- financiation-react/src/components/Groupcard.js
+++ financiation-react/src/components/Groupcard.js
@@ -1,45 +1,48 @@
 import React from 'react';
-import { Container, Row, Col, Card,Button } from 'react-bootstrap';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 import "../assets/styles/GroupCard.css"
 
 
 function GroupCard() {
   return (
     <Container fluid className="background">
+      <Container fluid>
+        titulo
+      </Container>
+
+
+
+      <Card className="CartaGroupcard">
         <Container fluid>
-            
+          <Row xs={2} md={2} >
+
+            <Col >Advisors</Col>
+            <Col className='columnas'>Coordinadores</Col>
+            </Row>
         </Container>
-        <Card className="CartaGroupcard">
-            
-            <Row xs={2} md={2} > 
-            <Container fluid className='cont1'>
-        <Col >Advisors</Col>
-        
-        </Container>
+
+
+
         
 
-        <Container fluid className='cont1'>
-        <Col >Coordinadores</Col>
-        </Container>
-        
         <Container fluid>
-        <Col >Ricardo</Col>
-        <Col >Juan</Col>
-        <Col >Pedro</Col>
+          <Col >Ricardo</Col>
+          <Col >Juan</Col>
+          <Col >Pedro</Col>
         </Container>
         <Container fluid>
-        <Col>Ricardito</Col>
-        <Col>Jose</Col>
-        <Col>Pepito</Col>
+          <Col>Ricardito</Col>
+          <Col>Jose</Col>
+          <Col>Pepito</Col>
         </Container>
-        
-      </Row>
-    
 
-        </Card>
-    </Container>
+      
+
+
+    </Card>
+    </Container >
   );
-  
+
 }
 
 export default GroupCard;
