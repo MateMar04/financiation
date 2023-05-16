d0effcf39452f0ce1643964fead39d1b40e4a4f6
mateosinches <71389954+mateosinches@users.noreply.github.com>
2023-05-16 08:23:26 -0300

    avance

--- financiation-react/src/containers/RegistroVisita.js
+++ financiation-react/src/containers/RegistroVisita.js
@@ -4,7 +4,7 @@ import "../assets/styles/registrovisita.css"
 import "primereact/resources/primereact.min.css";
 import Container from "react-bootstrap/esm/Container";
 import Button from 'react-bootstrap/Button';
-import { Form } from 'react-bootstrap';
+import { Form, Row } from 'react-bootstrap';
 
 export default function RegistroVisita() {
 
@@ -24,31 +24,50 @@ export default function RegistroVisita() {
         'Ciudad E',
     ];
 
+    const lugar = [
+        'Ciudad A',
+        'Ciudad B',
+        'Ciudad C',
+        'Ciudad D',
+        'Ciudad E',
+    ];
+
+
+
     return (
         <Container fluid>
-            <Container>
-                <row classname=" " id="Rowcontent">
-                    <container>
-                        <h2 id="tittle">Registro de Visitas</h2>
-                    </container>
-                    <container id="containerregi">
-                        <Form.Control id="input2" as="select"><option value="">Selecciona una localidad</option>{localidades.map((localidad, index) =>
-                            (<option key={index} value={localidad}>{localidad}</option>))}</Form.Control>
-                        <Form.Control id="input2" as="select"> {areasDeTrabajo.map((area, index) => (<option key={index}>{area}</option>))}</Form.Control>
-                        <Form.Control type="date" id="inputdate" name="dob" placeholder="Fecha" />
-                    </container>
-                    <container id="containerregi">
-                        <input type="text" id="inpu1" placeholder="Nombre" />
-                        <input type="text" id="inpu2" placeholder="Cargo" />
-                        <Form.Control id="inputnum" type="tel" placeholder="Numero" />
-                    </container>
-                    <container id="containerregi">
-                        <Form.Control id="inp1" type="time" />
-                        <input type="text" id="inp2" placeholder="Lugar" />
-                        <Button id="bt1" variant="primary">Registar</Button>{' '}
-                    </container>
-                </row>
-            </Container>
+            
+                <Container id="Rowcontent">
+                    <Container>
+                        <Row>
+                            <h2 id="tittle">Registro de Visitas</h2>
+                        </Row>
+                    </Container>
+                    <Container>
+                        <Row id="containerregi">
+                            <Form.Control id="input" as="select"><option value="">Localidad</option>{localidades.map((localidad, index) =>
+                                (<option key={index} value={localidad}>{localidad}</option>))}</Form.Control>
+                            <Form.Control id="input" as="select"> {areasDeTrabajo.map((area, index) => (<option key={index}>{area}</option>))}</Form.Control>
+                            <Form.Control type="date" id="inputdate" name="dob" placeholder="Fecha" />
+                        </Row>
+                    </Container>
+                    <Container>
+                        <Row id="containerregi">
+                            <Form.Control id="input" type="text" placeholder="Nombre" />
+                            <Form.Control id="input" type="text" placeholder="Cargo" />
+                            <Form.Control id="inputnum" type="tel" placeholder="Numero" />
+                        </Row>
+                    </Container>
+                    <Container>
+                        <Row id="containerregi">
+                            <Form.Control id="input" type="time" />
+                            <Form.Control id="input" as="select"><option value="">Lugar</option>{lugar.map((lugar, index) =>
+                                (<option key={index} value={lugar}>{lugar}</option>))}</Form.Control>
+                            <Button id="bt1" variant="primary">Registar</Button>{' '}
+                        </Row>
+                    </Container>
+                </Container>
+            
         </Container>
 
     )
