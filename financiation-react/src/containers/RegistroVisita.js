0bd2c829236e1f978d35504db2b51d94ad8a698c
mateosinches <71389954+mateosinches@users.noreply.github.com>
2023-05-17 08:36:51 -0300

    avance

--- financiation-react/src/containers/RegistroVisita.js
+++ financiation-react/src/containers/RegistroVisita.js
@@ -8,14 +8,6 @@ import { Form, Row } from 'react-bootstrap';
 
 export default function RegistroVisita() {
 
-    const areasDeTrabajo = [
-        'Ventas',
-        'Marketing',
-        'Desarrollo',
-        'Recursos Humanos',
-        'Operaciones',
-    ];
-
     const localidades = [
         'Ciudad A',
         'Ciudad B',
@@ -32,42 +24,117 @@ export default function RegistroVisita() {
         'Ciudad E',
     ];
 
+    const grupo = [
+        'Grupo 1',
+        'Grupo 2',
+        'Grupo 3',
+        'Grupo 4',
+        'Grupo 5',
+    ];
 
+    const visitstatus = [
+        'Confirmado',
+        'Pendiente',
+    ];
+
+    const agreement = [
+        'Si',
+        'No',
+    ];
+
+    const contactedreferrer = [
+        'Ventas',
+        'Marketing',
+        'Desarrollo',
+        'Recursos Humanos',
+        'Operaciones',
+    ];
+
+    const address = [
+        'Si',
+        'No',
+    ];
+
+    const logo = [
+        'Si',
+        'No',
+    ];
 
     return (
-        <Container fluid>
-            
-                <Container id="Rowcontent">
-                    <Container>
-                        <Row>
-                            <h2 id="tittle">Registro de Visitas</h2>
-                        </Row>
-                    </Container>
-                    <Container>
-                        <Row id="containerregi">
-                            <Form.Control id="input" as="select"><option value="">Localidad</option>{localidades.map((localidad, index) =>
-                                (<option key={index} value={localidad}>{localidad}</option>))}</Form.Control>
-                            <Form.Control id="input" as="select"> {areasDeTrabajo.map((area, index) => (<option key={index}>{area}</option>))}</Form.Control>
-                            <Form.Control type="date" id="inputdate" name="dob" placeholder="Fecha" />
-                        </Row>
-                    </Container>
-                    <Container>
-                        <Row id="containerregi">
-                            <Form.Control id="input" type="text" placeholder="Nombre" />
-                            <Form.Control id="input" type="text" placeholder="Cargo" />
-                            <Form.Control id="inputnum" type="tel" placeholder="Numero" />
-                        </Row>
-                    </Container>
-                    <Container>
-                        <Row id="containerregi">
-                            <Form.Control id="input" type="time" />
-                            <Form.Control id="input" as="select"><option value="">Lugar</option>{lugar.map((lugar, index) =>
-                                (<option key={index} value={lugar}>{lugar}</option>))}</Form.Control>
-                            <Button id="bt1" variant="primary">Registar</Button>{' '}
-                        </Row>
-                    </Container>
+        <Container fluid id="RegistroVisita">
+
+            <Container id="Rowcontent">
+                <Container>
+                    <Row>
+                        <h2 id="tittle">Registro de Visitas</h2>
+                    </Row>
                 </Container>
-            
+                <Container>
+                    <Row>
+                        <Form.Control id="input" type="text" placeholder="Flyer" />
+                        <Form.Control id="input" type="text" placeholder="Distancia" />
+                        <Form.Control id="input" type="time" placeholder="Tiempo de Viaje" />
+                    </Row>
+                </Container>
+                <Container>
+                    <Row >
+                        <Form.Control type="date" id="input" name="dob" placeholder="Fecha Visita" />
+                        <Form id="input">
+                            <Form.Check  >
+                                <Form.Check.Input />
+                                <Form.Check.Label>Registro Civil</Form.Check.Label>
+                            </Form.Check>
+                        </Form>
+                        <Form.Control id="input" type="text" placeholder="Hospedaje" />
+                    </Row>
+                </Container>
+                <Container>
+                    <Row >
+                        <Form id="input">
+                            <Form.Check  >
+                                <Form.Check.Input />
+                                <Form.Check.Label>Modernization Fund</Form.Check.Label>
+                            </Form.Check>
+                        </Form>
+                        <Form.Control id="input" type="time" placeholder="Start Time" />
+                        <Form.Control id="input" type="time" placeholder="Finish Time" />
+                    </Row>
+                </Container>
+                <Container>
+                    <Row >
+                        <Form.Control id="input" as="select"><option value="">Lugar</option>{lugar.map((lugar, index) =>
+                            (<option key={index} value={lugar}>{lugar}</option>))}</Form.Control>
+                        <Form.Control id="input" as="select"><option value="">Localidad</option>{localidades.map((localidad, index) =>
+                            (<option key={index} value={localidad}>{localidad}</option>))}</Form.Control>
+                        <Form.Control id="input" as="select"><option value="">Grupo</option>{grupo.map((grupo, index) =>
+                            (<option key={index} value={grupo}>{grupo}</option>))}</Form.Control>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        <Form.Control id="input" as="select"><option value="">Visit Status</option>{visitstatus.map((visitstatus, index) =>
+                            (<option key={index} value={visitstatus}>{visitstatus}</option>))}</Form.Control>
+                        <Form.Control id="input" as="select"><option value="">Agreement</option>{agreement.map((agreement, index) =>
+                            (<option key={index} value={agreement}>{agreement}</option>))}</Form.Control>
+                        <Form.Control id="input" as="select"><option value="">Contacto Referente</option>{contactedreferrer.map((contactedreferrer, index) =>
+                            (<option key={index} value={contactedreferrer}>{contactedreferrer}</option>))}</Form.Control>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        <Form.Control id="input" as="select"><option value="">Address</option>{address.map((address, index) =>
+                            (<option key={index} value={address}>{address}</option>))}</Form.Control>
+                        <Form.Control id="input" as="select"><option value="">Logo</option>{logo.map((logo, index) =>
+                            (<option key={index} value={logo}>{logo}</option>))}</Form.Control>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row className="justify-content-center align-items-center">
+                        <Button id="bt1" variant="primary">Registar</Button>{' '}
+                    </Row>
+                </Container>
+            </Container>
+
         </Container>
 
     )
