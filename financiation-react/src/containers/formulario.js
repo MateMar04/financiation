3881779412aa777a8910ead97b7f558bb3629dc3
LautaroVelez <lautivelez28@gmail.com>
2023-05-17 11:51:40 -0300

    css added

--- financiation-react/src/containers/formulario.js
+++ financiation-react/src/containers/formulario.js
@@ -2,7 +2,13 @@ import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import "../assets/styles/formulario.css"
+import "../assets/styles/formulario.css";
+
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+
 
 
 const Formulario = () => {
@@ -13,23 +19,34 @@ const Formulario = () => {
     };
 
     return (
+    <div>
         <Container className="general1">
             <Row className="justify-content-md-center">
-            <Col md="3">
-            <select value={selectedOption} onChange={handleDropdownChange} className="dropdowAreas">
-                <option value="">Seleccionar opción</option>
-                <option value="componente1">Componente 1</option>
-                <option value="componente2">Componente 2</option>
-            </select>
+                <Col md="11">
+                    <h1>Elegir area</h1>
+                    <FormControl sx={{ m: 1, minWidth: 800 }}>
+                        <InputLabel>Area</InputLabel>
+                        <Select
+                            value={selectedOption}
+                            onChange={handleDropdownChange}
+                            autoWidth
+                            label="Area"
+                        >
 
-            {selectedOption === "componente1" && <Componente1 />}
-            {selectedOption === "componente2" && <Componente2 />}
-            </Col>
-                </Row>
-
+                <MenuItem value="componente1">Componente 1</MenuItem>
+                <MenuItem value="componente2">Componente 2</MenuItem>
+            </Select>
+        </FormControl>
+                </Col>
+            </Row>
         </Container>
+
+        {selectedOption === "componente1" && <Componente1 />}
+        {selectedOption === "componente2" && <Componente2 />}
+    </div>
     );
 };
+
 
 const Componente1 = () => {
     return <h2>Componente 1</h2>;
@@ -40,3 +57,4 @@ const Componente2 = () => {
 };
 
 export default Formulario;
+
