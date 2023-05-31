9d12f1e433c024a3714a54796ede3979adc0aa9a
Mateo Marchisone <mateo.marchisone@gmail.com>
2023-05-31 08:23:17 -0300

    Various AdminModels

--- financiationDjango/djangoapp/admin.py
+++ financiationDjango/djangoapp/admin.py
@@ -38,3 +38,38 @@ class AddressAdmin(admin.ModelAdmin):
     search_fields = ('id', 'street', 'number')
     list_filter = ('id', 'street', 'number')
     list_per_page = 10
+
+
+class UserStatusAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description')
+    search_fields = ('id', 'name', 'description')
+    list_filter = ('id', 'name', 'description')
+    list_per_page = 10
+
+
+class VisitStatusAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description')
+    search_fields = ('id', 'name', 'description')
+    list_filter = ('id', 'name', 'description')
+    list_per_page = 10
+
+
+class FaqAdmin(admin.ModelAdmin):
+    list_display = ('id', 'faq')
+    search_fields = ('id', 'faq')
+    list_filter = ('id', 'faq')
+    list_per_page = 10
+
+
+class MayorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name')
+    search_fields = ('id', 'first_name', 'last_name')
+    list_filter = ('id', 'first_name', 'last_name')
+    list_per_page = 10
+
+
+class LocalityAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'id_department')
+    search_fields = ('id', 'name', 'id_department')
+    list_filter = ('id', 'name', 'id_department')
+    list_per_page = 10
