85bdf4c73df8e861a7ac0720be276f37d441dc1c
Mateo Marchisone <mateo.marchisone@gmail.com>
2023-05-31 08:20:57 -0300

    Various AdminModels

--- financiationDjango/djangoapp/admin.py
+++ financiationDjango/djangoapp/admin.py
@@ -10,3 +10,31 @@ class AdvisedAdmin(admin.ModelAdmin):
     search_fields = ('id', 'ssn', 'first_name', 'last_name',)
     list_filter = ('id', 'ssn', 'first_name', 'last_name',)
     list_per_page = 10
+
+
+class AgreementAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description',)
+    search_fields = ('id', 'name', 'description',)
+    list_filter = ('id', 'name', 'description',)
+    list_per_page = 10
+
+
+class CityDepartmentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description')
+    search_fields = ('id', 'name', 'description')
+    list_filter = ('id', 'name', 'description')
+    list_per_page = 10
+
+
+class MinistryDepartmentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description')
+    search_fields = ('id', 'name', 'description')
+    list_filter = ('id', 'street', 'number')
+    list_per_page = 10
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('id', 'street', 'number')
+    search_fields = ('id', 'street', 'number')
+    list_filter = ('id', 'street', 'number')
+    list_per_page = 10
