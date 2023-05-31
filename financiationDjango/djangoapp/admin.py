a80d503622f65cb8f49acf05ce6c73751b102811
Mateo Marchisone <mateo.marchisone@gmail.com>
2023-05-31 08:14:49 -0300

    AdvisedAdmin

--- financiationDjango/djangoapp/admin.py
+++ financiationDjango/djangoapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+
+class AdvisedAdmin(admin.ModelAdmin):
+    list_display = ('id', 'ssn', 'first_name', 'last_name',)
+    search_fields = ('id', 'ssn', 'first_name', 'last_name',)
+    list_filter = ('id', 'ssn', 'first_name', 'last_name',)
+    list_per_page = 10
