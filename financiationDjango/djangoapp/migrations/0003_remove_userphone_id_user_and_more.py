8800191c376da143a26c267a08bf5928c3725c06
Mateo Marchisone <mateo.marchisone@gmail.com>
2023-05-31 09:51:38 -0300

    Modified Migrations

--- financiationDjango/djangoapp/migrations/0003_remove_userphone_id_user_and_more.py
+++ financiationDjango/djangoapp/migrations/0003_remove_userphone_id_user_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('djangoapp', '0002_auto_20230516_1159'),
+        ('djangoapp', '0002_get_localities_and_departments'),
     ]
 
     operations = [
