f8461faec64e3f00de16405ab1b3c20d51402f09
LorenzoGalaverna <lorenzo.galaverna1@gmail.com>
2023-06-01 09:44:04 -0300

    advancesss

--- financiationDjango/djangoapp/serializers.py
+++ financiationDjango/djangoapp/serializers.py
@@ -4,21 +4,13 @@ from djoser.serializers import UserCreateSerializer
 from .models import Visit
 
 class VisitSerializer(serializers.ModelSerializer) :
-        
-    id_locality = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-    id_group = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-    id_visit_status = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-    id_agreement = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-    id_contacter_referrer = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-    id_address = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-    id_logo = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
     class Meta:
         model = Visit
         fields = ('flyer', 'distance', 'travel_time', 'visit_date', 'civil_registration',
                     'accommodation', 'modernization_fund', 'start_time', 'finish_time',
                     'place_name', 'id_locality', 'id_group', 'id_visit_status', 'id_agreement',
-                    'id_contacted_referrer', 'id_address', 'id_logo')
+                     'id_address', 'id_logo', 'id_contacted_referrer')
 
 User = get_user_model()
 
