8c39376f3aa77772307882a774e623c7dc79d7f0
Mateo Marchisone <mateo.marchisone@gmail.com>
2023-06-03 00:42:59 -0300

    Created Advised views

--- financiationDjango/financiationAPI/views.py
+++ financiationDjango/financiationAPI/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Advised
+from .serializers import AdvisedSerializer
+
 
 # Create your views here.
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        {
+            'Endpoint': '/advised/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns an array of advised'
+        },
+    ]
+
+    return Response(routes)
+
+
+@api_view(['GET'])
+def getAdvised(request):
+    advised = Advised.objects.all()
+    serializer = AdvisedSerializer(advised, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getOneAdvised(request, pk):
+    advised = Advised.objects.get(id=pk)
+    serializer = AdvisedSerializer(advised, many=False)
+    return Response(serializer.data)
