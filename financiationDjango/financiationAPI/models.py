14a74b72b1b6fafa27d6b9033d59c982e77a0001
Mateo Marchisone <mateo.marchisone@gmail.com>
2023-06-03 00:41:55 -0300

    Create Advised model

--- financiationDjango/financiationAPI/models.py
+++ financiationDjango/financiationAPI/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Advised(models.Model):
+    ssn = models.BigIntegerField()
+    first_name = models.CharField(max_length=70)
+    last_name = models.CharField(max_length=70)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
