3cc2e873bf5fbf415331ea51c5cc6f05160dea6e
Mateo Marchisone <mateo.marchisone@gmail.com>
2023-06-03 15:52:25 -0300

    UserAccount serializer

--- financiationDjango/financiationAPI/serializers.py
+++ financiationDjango/financiationAPI/serializers.py
@@ -1,8 +1,25 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Advised
+from django.contrib.auth import get_user_model
+from djoser.serializers import UserCreateSerializer
+from .models import *
 
 
 class AdvisedSerializer(ModelSerializer):
     class Meta:
         model = Advised
         fields = '__all__'
+
+
+class VisitSerializer(ModelSerializer):
+    class Meta:
+        model = Visit
+        fields = '__all__'
+
+
+User = get_user_model()
+
+
+class UserCreateSerializer(UserCreateSerializer):
+    class Meta(UserCreateSerializer.Meta):
+        model = User
+        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'ssn', 'phone_number')
