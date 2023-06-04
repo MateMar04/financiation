9eb317bdf3b1dc9bb7fcb05bb06ec44571ea68df
Mateo Marchisone <mateo.marchisone@gmail.com>
2023-06-04 18:47:55 -0300

    Changed tokens lifetime

--- financiation-react/src/pages/AdvisedListPage.jsx
+++ financiation-react/src/pages/AdvisedListPage.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import ListAdvised from "../components/ListAdvised";
 
 const AdvisedListPage = () => {
--- financiationDjango/financiationDjango/settings.py
+++ financiationDjango/financiationDjango/settings.py
@@ -53,8 +53,8 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=90),
 }
 
 DJOSER = {
