d552e67bd6761a797598b164a3bd5d236d67e957
Mateo Marchisone <mateo.marchisone@gmail.com>
2023-06-04 14:48:04 -0300

    Added ActivateAccountPage.jsx

---
+++ financiation-react/src/pages/ActivateAccountPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import '../assets/styles/LoginPage.css'
+
+const ActivateAccountPage = () => {
+    return (
+        <Container fluid>
+            <h1>Verify your account</h1>
+            <Button>Verify</Button>
+        </Container>
+    )
+}
+
+export default ActivateAccountPage
