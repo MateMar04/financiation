b9a52ce0bdc4ded49fdf20776e84cf4625883b90
Mateo Marchisone <mateo.marchisone@gmail.com>
2023-06-04 20:01:21 -0300

    authTokens in contextData

--- financiation-react/src/context/AuthContext.jsx
+++ financiation-react/src/context/AuthContext.jsx
@@ -62,6 +62,7 @@ export const AuthProvider = ({children}) => {
 
     let contextData = {
         user: user,
+        authTokens: authTokens,
         loginUser: loginUser,
         logoutUser: logoutUser
     }
