63e6a1ccc8eb2d930bdb00e6622140de1d63f8ff
Mateo Marchisone <mateo.marchisone@gmail.com>
2023-06-04 14:50:19 -0300

    Added ResetPasswordConfirmPage.jsx

---
+++ financiation-react/src/pages/ResetPasswordConfirmPage.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import {Button, Container,} from "react-bootstrap";
+import Form from "react-bootstrap/Form";
+
+const ResetPasswordConfirmPage = () => {
+    return (
+        <Container className='mt-5'>
+            <Form onSubmit=''>
+                <Form.Group>
+                    <Form.Control
+                        type='password'
+                        placeholder='Confirm New Password'
+                        name='new_password'
+                        value=''
+                        onChange=''
+                        minLength='6'
+                        required
+                    />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Control
+                        type='password'
+                        placeholder='New Password'
+                        name='re_new_password'
+                        value=''
+                        onChange=''
+                        minLength='6'
+                        required
+                    />
+                </Form.Group>
+                <Button className='btn btn-primary' type='submit'>Reset Password</Button>
+            </Form>
+        </Container>
+    );
+}
+
+export default ResetPasswordConfirmPage
