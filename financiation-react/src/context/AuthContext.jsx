df5cb41d2da985a645de4a8b5660010671c21fab
Mateo Marchisone <mateo.marchisone@gmail.com>
2023-06-04 20:01:07 -0300

    updateToken

--- financiation-react/src/context/AuthContext.jsx
+++ financiation-react/src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import {createContext, useState} from "react";
+import {createContext, useEffect, useState} from "react";
 import jwt_decode from "jwt-decode";
 import {useNavigate} from 'react-router-dom'
 
@@ -9,6 +9,7 @@ export const AuthProvider = ({children}) => {
 
     let [authTokens, setAuthTokens] = useState(() => localStorage.getItem('authTokens') ? JSON.parse(localStorage.getItem('authTokens')) : null);
     let [user, setUser] = useState(() => localStorage.getItem('authTokens') ? jwt_decode(localStorage.getItem('authTokens')) : null);
+    let [loading, setLoading] = useState(true)
 
     let history = useNavigate()
 
@@ -39,11 +40,41 @@ export const AuthProvider = ({children}) => {
         history('/')
     }
 
+    let updateToken = async () => {
+        console.log('Update')
+        let response = await fetch('/auth/jwt/refresh', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({'refresh': authTokens.refresh})
+        })
+        let data = await response.json()
+
+        if (response.status === 200) {
+            setAuthTokens(data)
+            setUser(jwt_decode(data.access))
+            localStorage.setItem('authTokens', JSON.stringify(data))
+        } else {
+            logoutUser()
+        }
+    }
+
     let contextData = {
         user: user,
         loginUser: loginUser,
         logoutUser: logoutUser
     }
+
+    useEffect(() => {
+        let fourMinutes = 1000 * 60 * 4
+        let interval = setInterval(() => {
+            if (authTokens) {
+                updateToken()
+            }
+        }, fourMinutes)
+        return () => clearInterval(interval)
+    }, [authTokens, loading])
 
 
     return (
