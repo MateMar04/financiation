0b0d6ee48b445c866dfc6aae0fdd6f32714f923d
LautaroVelez <lautivelez28@gmail.com>
2023-06-04 22:01:50 -0300

    (Navbar) button login to the right (poorly accomplished)

--- financiation-react/src/components/Navbar.js
+++ financiation-react/src/components/Navbar.js
@@ -1,33 +1,41 @@
-import React, {Fragment} from "react"
+import React, { Fragment } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import "../assets/styles/navbar.css"
+import "../assets/styles/navbar.css";
 import logofinanzas from '../assets/images/logofinanzas.png';
-import {Link} from "react-router-dom";
-import {connect} from "react-redux";
-import {logout} from "../actions/auth";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { logout } from "../actions/auth";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-
-const NavigationBar = ({logout, isAuthenticated}) => {
-
+const NavigationBar = ({ logout, isAuthenticated }) => {
     const guestLinks = () => (
-        <Fragment>
-            <Nav.Item>
-                <Button variant="outline-light" to="/login/" className='navlinks' href='/login'>Iniciar Sesion</Button>
-            </Nav.Item>
-            <Nav.Item>
-                <Button variant="outline-light" to="/signup/" className='navlinks' href='/signup'>Crear Cuenta</Button>
-            </Nav.Item>
-        </Fragment>
+        <>
+            <Nav>
+                <Nav.Item>
+                <Button variant="outline-light" className="navLinkLogin" href="/login">
+                    Iniciar Sesión
+                </Button>
+                </Nav.Item>
+                <Nav.Item>
+                <Button variant="outline-light" className="navLinkSignup" href="/signup">
+                    Crear Cuenta
+                </Button>
+                </Nav.Item>
+            </Nav>
+        </>
     );
 
     const authLinks = () => (
         <Nav.Item>
-            <a className="nav-link" href="#!" onClick={logout}>Cerrar Sesion</a>
+            <a className="nav-link" href="#!" onClick={logout}>
+                Cerrar Sesión
+            </a>
         </Nav.Item>
     );
 
@@ -35,28 +43,23 @@ const NavigationBar = ({logout, isAuthenticated}) => {
         <Navbar expand="lg" id="navbarcs" className="navbarcs">
             <Container fluid>
                 <Link to="/">
-                    <Navbar.Brand id="logoboton" alt='logoboton'><img src={logofinanzas} id='logoboton'
-                                                                      alt="logo de ministerio de finanzas"/>
+                    <Navbar.Brand id="logoboton" alt='logoboton'>
+                        <img src={logofinanzas} id='logoboton' alt="logo de ministerio de finanzas" />
                     </Navbar.Brand>
                 </Link>
-
-                <Navbar.Toggle aria-controls="navbarScroll"/>
-
+                <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse>
-                    <Nav className="me-auto my-2 my-lg-0">
-
+                    <Nav>
                         {isAuthenticated ? authLinks() : guestLinks()}
-
                     </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
-
     );
-}
+};
 
 const mapStateToProps = state => ({
     isAuthenticated: state.auth.isAuthenticated
 });
 
-export default connect(mapStateToProps, {logout})(NavigationBar);
+export default connect(mapStateToProps, { logout })(NavigationBar);
