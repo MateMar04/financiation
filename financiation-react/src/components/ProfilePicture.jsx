2982d5035fa9c5e08d1b5d9aef04f2b62e184578
Mateo Marchisone <mateo.marchisone@gmail.com>
2023-06-12 22:35:03 -0300

    Profile Picture

---
+++ financiation-react/src/components/ProfilePicture.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {Container} from "react-bootstrap";
+import '../assets/styles/ProfilePicture.css'
+
+export const ProfilePicture = ({username, profileImg}) => {
+    return (
+        <Container className="profile-picture">
+            <img src={profileImg} alt={username} className="responsive-image"/>
+        </Container>
+    )
+}
+
+export default ProfilePicture
