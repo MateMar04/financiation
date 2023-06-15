9b75ef1d8b2015706c0ce626fa2f785dc6fe3c27
Julian Rosas <julymaxrosas@gmail.com>
2023-06-15 08:50:21 -0300

    empezando jsx

---
+++ financiation-react/src/pages/CreateGroup.jsx
@@ -0,0 +1,10 @@
+import React, {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
+import "../assets/styles/AddVisitPage.css"
+import {Button, Container, Form} from "react-bootstrap";
+
+const navigate = useNavigate;
+
+const CreateGroupPage = () => {
+    
+} 
