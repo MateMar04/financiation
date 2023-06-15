ce3fcf96f6f41458f15bf62df53fae20e17eb7f1
LorenzoGalaverna <lorenzo.galaverna1@gmail.com>
2023-06-15 12:01:39 -0300

    serrializers updated

--- financiationDjango/financiationAPI/serializers.py
+++ financiationDjango/financiationAPI/serializers.py
@@ -4,6 +4,12 @@ from djoser.serializers import UserCreateSerializer
 from .models import *
 
 
+class RequestSerializer(ModelSerializer):
+    class Meta:
+        model=Request
+        fields = '__all__'
+
+
 class AdvisedSerializer(ModelSerializer):
     class Meta:
         model = Advised
@@ -16,10 +22,47 @@ class VisitSerializer(ModelSerializer):
         fields = '__all__'
 
 
-class RequestSerializer(ModelSerializer):
+class GroupSerializer(ModelSerializer):
     class Meta:
-        model=Request
+        model = Group
         fields = '__all__'
+
+
+class LocalitySerializer(ModelSerializer):
+    class Meta:
+        model = Locality
+        fields = '__all__'
+
+
+class VisitStatusSerializer(ModelSerializer):
+    class Meta:
+        model = VisitStatus
+        fields = '__all__'
+
+
+class AgreementSerializer(ModelSerializer):
+    class Meta:
+        model = Agreement
+        fields = '__all__'
+
+
+class ContactedReferrerSerializer(ModelSerializer):
+    class Meta:
+        model = ContactedReferrer
+        fields = '__all__'
+
+
+class AddressSerializer(ModelSerializer):
+    class Meta:
+        model = Address
+        fields = '__all__'
+
+
+class LogoSerializer(ModelSerializer):
+    class Meta:
+        model = Logo
+        fields = '__all__'
+
 
 
 User = get_user_model()
@@ -29,3 +72,4 @@ class UserCreateSerializer(UserCreateSerializer):
     class Meta(UserCreateSerializer.Meta):
         model = User
         fields = ('id', 'username', 'email', 'first_name', 'last_name', 'ssn', 'phone_number')
+
