57756ce54469820294781a127168a471e063c288
LorenzoGalaverna <lorenzo.galaverna1@gmail.com>
2023-06-15 11:59:19 -0300

    urls updated

--- financiationDjango/financiationAPI/urls.py
+++ financiationDjango/financiationAPI/urls.py
@@ -5,4 +5,14 @@ urlpatterns = [
     path('', views.getRoutes, name='routes'),
     path('advised/', views.getAdvised, name='advised'),
     path('advised/<str:pk>/', views.getOneAdvised, name='oneAdvised'),
+    path('locality/', views.getLocalities, name='getLocalities'),
+    path('group/', views.getGroups, name='getGroups'),
+    path('visit-status/', views.getVisitSatuses, name='getVisitSatuses'),
+    path('agreement/', views.getAgreements, name='getAgreements'),
+    path('contacted-referrer/', views.getContactedReferrers, name='getContactedReferrers'),
+    path('address/', views.getAddresses, name='getAddresses'),
+    path('logo/', views.getLogos, name='getLogos'),
+    path('visit/add/', views.postVisit, name='postVisit'),
+    path('group/add/', views.postGroup, name='postGroup'),
+    path('visit/', views.getVisits, name='getVisits')
 ]
