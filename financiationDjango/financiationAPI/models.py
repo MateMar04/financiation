dc59c1c8d1171614b88b72877dcf860f79390ab5
Mateo Marchisone <mateo.marchisone@gmail.com>
2023-06-19 20:33:56 -0300

    Load my profile info

--- financiationDjango/financiationAPI/models.py
+++ financiationDjango/financiationAPI/models.py
@@ -150,14 +150,15 @@ class UserAccountManager(BaseUserManager):
 
         return user
 
-    def create_superuser(self, username, email, first_name, last_name, ssn, phone_number, password=None, is_staff=True,
-                         is_superuser=True):
+    def create_superuser(self, username, email, first_name, last_name, ssn, phone_number, profile_picture,
+                         password=None, is_staff=True, is_superuser=True):
         if not email:
             raise ValueError("Users must have an email address")
 
         email = self.normalize_email(email)
         user = self.model(username=username, email=email, first_name=first_name, last_name=last_name, ssn=ssn,
-                          is_staff=is_staff, phone_number=phone_number, is_superuser=is_superuser)
+                          is_staff=is_staff, phone_number=phone_number, is_superuser=is_superuser,
+                          profile_picture=profile_picture)
 
         user.set_password(password)
         user.save()
@@ -182,7 +183,7 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
     objects = UserAccountManager()
 
     USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['first_name', 'last_name', 'email', 'ssn', 'phone_number']
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'email', 'ssn', 'phone_number', 'profile_picture']
 
     def get_full_name(self):
         return f"{self.first_name} {self.last_name}"
