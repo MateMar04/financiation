4e2eb605d1016afcad17bcb93995845acf4b5b7c
Mateo Marchisone <mateo.marchisone@gmail.com>
2023-06-20 13:25:18 -0300

    Scrollspy

--- financiation-react/src/pages/ReportsPage.jsx
+++ financiation-react/src/pages/ReportsPage.jsx
@@ -1,17 +1,35 @@
 import React from 'react'
-import {Col, Container, Row} from "react-bootstrap";
+import {Col, Container, Form, Row} from "react-bootstrap";
 import "../assets/styles/ReportsPage.css"
+
+
+const rows = []
+for (let i = 0; i <= 30; i++) {
+    rows.push(
+        <Row key={i}>
+            <Col lg={6}>
+                <Form.Label>Ciudad {i}</Form.Label>
+            </Col>
+            <Col lg={6}>
+                <Form.Check></Form.Check>
+            </Col>
+        </Row>)
+}
+
 
 export const ReportsPage = () => {
     return (
         <Container fluid>
             <Row>
-                <Col lg='4' className='red'>
-                    <Container>
-
+                <Col lg='4'>
+                    <Container className='red'>
+                        <h1 className='title'>Selecciona Localidad</h1>
+                        <Form>
+                            {rows}
+                        </Form>
                     </Container>
                 </Col>
-                <Col lg='8' className='blue'>
+                <Col lg='8'>
                     <Container className='green'></Container>
                     <Container className='yellow'></Container>
                 </Col>
@@ -19,3 +37,5 @@ export const ReportsPage = () => {
         </Container>
     )
 }
+
+export default ReportsPage
