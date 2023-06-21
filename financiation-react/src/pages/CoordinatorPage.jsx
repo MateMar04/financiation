e8fd9a0dfecf6d5c7938c38fbe12c3da6dd470cd
LorenzoGalaverna <lorenzo.galaverna1@gmail.com>
2023-06-21 09:00:18 -0300

    a

--- financiation-react/src/pages/CoordinatorPage.jsx
+++ financiation-react/src/pages/CoordinatorPage.jsx
@@ -1,29 +1,28 @@
-import React, {useState} from 'react';
-import {useNavigate} from 'react-router-dom';
+import React, {useContext, useEffect, useState} from 'react';
+import "../assets/styles/AddVisitPage.css"
 import {Button, Container, Form} from "react-bootstrap";
-
-const navigate = useNavigate;
+import AuthContext from "../context/AuthContext";
 
 const CoordinatorPage = () => {
     const [id_user ,setUser] = useState("");
     const [id_group ,setGroup] = useState("");
 
-    let [request, setRequest] = useState(null)
+    let [coordinator, setCoordinator] = useState(null)
     let {authTokens, logoutUser} = useContext(AuthContext)
 
     useEffect(() => {
-        setRequest()
+        setCoordinator()
     })
 
-    let getpostCoordinator = async () => {
-        fetch(' /api/Coordinator/add/', {
+    let postCoordinator = async () => {
+        fetch(' /api/coordinator/add/', {
             method: "POST",
             headers: {
                 'Content-Type': 'application/json',
                 "Authorization": "JWT " + String(authTokens.access),
                 "Accept": "application/json"
             },
-            body: JSON.stringify(Coordinator)
+            body: JSON.stringify(coordinator)
         })
     }
     return (
@@ -52,10 +51,10 @@ const CoordinatorPage = () => {
                 </Form.Group>
             </Form>
             <Form.Group>
-                <Button onClick={CreateGroupInfo}>Submit</Button>
+                <Button onClick={postCoordinator}>Submit</Button>
             </Form.Group>
         </Container>
     );
 
 };
-export default CreateGroupPage;
+export default CoordinatorPage;
