a02c7f60f55ac6002269b6bf08ff68603993defe
Mateo Marchisone <mateo.marchisone@gmail.com>
2023-06-20 23:28:41 -0300

    Changed upload direction

--- financiationDjango/financiationAPI/models.py
+++ financiationDjango/financiationAPI/models.py
@@ -176,7 +176,7 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
     phone_number = models.BigIntegerField()
-    profile_picture = models.ImageField(upload_to='financiationAPI/images/', default=None)
+    profile_picture = models.ImageField(default=None)
     id_role = models.ForeignKey(Role, models.DO_NOTHING, null=True)
     id_user_status = models.ForeignKey(UserStatus, models.DO_NOTHING, null=True)
 
