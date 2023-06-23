e4f7b764c188f7bdd0eb73dd49bb2411162b4001
Mateo Marchisone <mateo.marchisone@gmail.com>
2023-06-22 21:12:15 -0300

    fixed urls and package.json

--- financiationDjango/financiationDjango/urls.py
+++ financiationDjango/financiationDjango/urls.py
@@ -18,7 +18,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
