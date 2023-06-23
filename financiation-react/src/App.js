0607535a4611f5f2da168dff1dd9bc5e7e4a99d1
tadeofunes <71388800+tadeofunes@users.noreply.github.com>
2023-06-23 14:17:51 -0300

    Mas lindo.

--- financiation-react/src/App.js
+++ financiation-react/src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css';
 import Navbar from './components/NavbarComponent'
 import AdvisedListPage from './pages/AdvisedListPage'
@@ -11,13 +11,13 @@ import ResetPasswordConfirmPage from "./pages/ResetPasswordConfirmPage";
 import ActivateAccountPage from "./pages/ActivateAccountPage";
 import AddVisitPage from "./pages/AddVisitPage";
 import PrivateRoute from "./utils/PrivateRoute";
-import {AuthProvider} from "./context/AuthContext";
+import { AuthProvider } from "./context/AuthContext";
 import GroupCard from "./components/GroupCard";
 import FormPage from "./pages/FormPage";
 import FormRequestPage from "./pages/FormRequestPage";
 import ProfilePage from "./pages/ProfilePage";
-import {ReportsPage} from "./pages/ReportsPage";
-import {CreateGroupPage} from "./pages/CreateGroupPage";
+import { ReportsPage } from "./pages/ReportsPage";
+import { CreateGroupPage } from "./pages/CreateGroupPage";
 import React from "react";
 
 
@@ -26,63 +26,80 @@ function App() {
         <Router>
             <div className="App">
                 <AuthProvider>
-                    <Navbar/>
+                    <Navbar />
                     <Routes>
-                        <Route path='/' exact element={<PrivateRoute><LandingPage/></PrivateRoute>}/>
+                        <Route path='/' exact element={<PrivateRoute><LandingPage /></PrivateRoute>} />
 
-                        <Route path='/login' element={<LoginPage/>}/>
-                        <Route path='/signin' element={<SigninPage/>}/>
-                        <Route path='/reset-password' element={<ResetPasswordPage/>}/>
-                        <Route path='/reset-password/confirm' element={<ResetPasswordConfirmPage/>}/>
-                        <Route path='/activate' element={<ActivateAccountPage/>}/>
+                        <Route path='/login' element={<LoginPage />} />
+                        <Route path='/signin' element={<SigninPage />} />
+                        <Route path='/reset-password' element={<ResetPasswordPage />} />
+                        <Route path='/reset-password/confirm' element={<ResetPasswordConfirmPage />} />
+                        <Route path='/activate' element={<ActivateAccountPage />} />
 
-                        <Route exact path='/me' element={<ProfilePage/>}/>
+                        <Route exact path='/me' element={<ProfilePage />} />
 
-                        <Route path='/advised' exact element={<PrivateRoute><AdvisedListPage/></PrivateRoute>}/>
-                        <Route path='/advised/:id' element={<PrivateRoute><AdvisedPage/></PrivateRoute>}/>
+                        <Route path='/advised' exact element={<PrivateRoute><AdvisedListPage /></PrivateRoute>} />
+                        <Route path='/advised/:id' element={<PrivateRoute><AdvisedPage /></PrivateRoute>} />
 
-                        <Route path='/group' element={<GroupCard group={{id: 1, name: 'Pepe'}} advisors={[
-            {
-                "id": 1,
-                "id_user": "Mateo Marchisone",
-                "id_group": 1
-            },
-            {
-                "id": 2,
-                "id_user": "Elvio Marchisone",
-                "id_group": 2
-            },
-            {
-                "id": 3,
-                "id_user": "Tadeo Funes",
-                "id_group": 3
-            }
-        ]}
-                   coordinators={[
-                       {
-                           "id": 1,
-                           "id_user": "Mateo Marchisone",
-                           "id_group": 1
-                       },
-                       {
-                           "id": 2,
-                           "id_user": "Elvio Marchisone",
-                           "id_group": 2
-                       },
-                       {
-                           "id": 3,
-                           "id_user": "Tadeo Funes",
-                           "id_group": 3
-                       }
-                   ]}/>}/>
-                        <Route path='/form' element={<FormPage/>}></Route>
+                        <Route path='/group' element={<GroupCard group={{ id: 1, name: '15 de jorge' }} advisors={[
+                            {
+                                "id": 1,
+                                "id_user": "Mateo Marchisone",
+                                "id_group": 1
+                            },
+                            {
+                                "id": 2,
+                                "id_user": "Elvio Marchisone",
+                                "id_group": 2
+                            },
+                            {
+                                "id": 3,
+                                "id_user": "Tadeo Funes",
+                                "id_group": 3
+                            },
+                            {
+                                "id": 3,
+                                "id_user": "Tadeo Funes",
+                                "id_group": 3
+                            },
+                            {
+                                "id": 3,
+                                "id_user": "Tadeo Funes",
+                                "id_group": 3
+                            }
 
-                        <Route path='/visit/add' element={<AddVisitPage/>}/>
+                        ]}
+                            coordinators={[
+                                {
+                                    "id": 1,
+                                    "id_user": "Mateo Marchisone",
+                                    "id_group": 1
+                                },
+                                {
+                                    "id": 2,
+                                    "id_user": "Elvio Marchisone",
+                                    "id_group": 2
+                                },
+                                {
+                                    "id": 3,
+                                    "id_user": "Tadeo Funes",
+                                    "id_group": 3
+                                },
 
-                        <Route path='/request/add' element={<FormRequestPage/>}/>
-                        <Route path='/group/add' element={<CreateGroupPage/>}/>
+                                {
+                                    "id": 2,
+                                    "id_user": "Elvio Marchisone",
+                                    "id_group": 2
+                                }
+                            ]} />} />
+                        <Route path='/form' element={<FormPage />}></Route>
 
-                        <Route path='/reports' element={<ReportsPage/>}/>
+                        <Route path='/visit/add' element={<AddVisitPage />} />
+
+                        <Route path='/request/add' element={<FormRequestPage />} />
+                        <Route path='/group/add' element={<CreateGroupPage />} />
+
+                        <Route path='/reports' element={<ReportsPage />} />
 
                     </Routes>
                 </AuthProvider>
--- financiation-react/src/components/GroupCard.jsx
+++ financiation-react/src/components/GroupCard.jsx
@@ -3,21 +3,29 @@ import {Card, Col, Container, Row} from 'react-bootstrap';
 import "../assets/styles/GroupCard.css"
 
 
-export const GroupCard = ({group, advisors, coordinators, visita}) => {
+export const GroupCard = ({group, advisors, coordinators}) => {
     return (
-        <Container fluid>
-            <Card>
-                <h1>Grupo {group.name}</h1>
-                <Row>
-                    <Col>
-                        <h1>Asesores</h1>
+        <Container fluid className='CompletlyContainer'>
+            <Card className='groupcard'>
+            <Container fluid className='ContainerNameGroup'>
+                <h2 className='b'>Grupo numero {group.name}</h2>
+                </Container>
+                <Row className='Columna1'>
+
+                    <Col >
+                    <Container>
+                        <h3 className='a'>Asesores</h3>
+                        </Container>
                         {advisors?.map((advisor) => (
                           <p>{advisor.id_user}</p>
                         ))}
                     </Col>
                     <Col>
-                        <h1>Coordinadores</h1>
-                        {coordinators?.map((coordinator) => (
+                    
+                    <Container>
+                        <h3 className='a'>Coordinadores</h3>
+                        </Container>
+                        {coordinators?.map  ((coordinator) => (
                           <p>{coordinator.id_user}</p>
                         ))}
                     </Col>
@@ -25,6 +33,7 @@ export const GroupCard = ({group, advisors, coordinators, visita}) => {
             </Card>
 
         </Container>
+        
     )
 }
 
