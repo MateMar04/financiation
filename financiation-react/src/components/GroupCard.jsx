3c3e136d824b5ebe31a768e5393f1ed0049ff3d1
Mateo Marchisone <mateo.marchisone@gmail.com>
2023-06-25 20:17:49 -0300

    Proper data load on groupCard

--- financiation-react/src/components/GroupCard.jsx
+++ financiation-react/src/components/GroupCard.jsx
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {Card, Col, Container, Row} from 'react-bootstrap';
 import "../assets/styles/GroupCard.css"
 import {UserParagraph} from "./UserParagraph";
+import AuthContext from "../context/AuthContext";
 
 
-export const GroupCard = ({group, advisors, coordinators}) => {
+
+
+export const GroupCard = ({group}) => {
+
+    let {authTokens} = useContext(AuthContext)
+    let [coordinators, setCoordinators] = useState([])
+    let [advisors, setAdvisors] = useState([])
+
+
+
+    useEffect(() => {
+        getGroupAdvisors()
+        getGroupCoordinators()
+    }, [])
+    let getGroupCoordinators = async () => {
+        let headers = {
+            "Content-Type": "application/json",
+            "Authorization": "JWT " + String(authTokens.access),
+            "Accept": "application/json"
+        }
+        let response = await fetch(`/api/groups/${group.id}/coordinators`, {headers: headers})
+        let data = await response.json()
+        setCoordinators(data)
+    };
+
+    let getGroupAdvisors = async () => {
+        let headers = {
+            "Content-Type": "application/json",
+            "Authorization": "JWT " + String(authTokens.access),
+            "Accept": "application/json"
+        }
+        let response = await fetch(`/api/groups/${group.id}/advisors`, {headers: headers})
+        let data = await response.json()
+        setAdvisors(data)
+    };
+
+
     return (
         <Container fluid className='CompletlyContainer'>
             <Card className='groupcard'>
--- financiation-react/src/pages/GroupsPage.jsx
+++ financiation-react/src/pages/GroupsPage.jsx
@@ -7,38 +7,12 @@ import {Container} from "react-bootstrap";
 export const GroupsPage = () => {
 
     let {authTokens} = useContext(AuthContext)
-    let [advisors, setAdvisors] = useState([])
-    let [coordinators, setCoordinators] = useState([])
     let [groups, setGroups] = useState([])
     let history = useNavigate()
 
     useEffect(() => {
-        getAdvisors()
-        getCoordinators()
         getGroups()
     }, [])
-
-    let getAdvisors = async () => {
-        let headers = {
-            "Content-Type": "application/json",
-            "Authorization": "JWT " + String(authTokens.access),
-            "Accept": "application/json"
-        }
-        let response = await fetch('/api/advisor/', {headers: headers})
-        let data = await response.json()
-        setAdvisors(data)
-    };
-
-    let getCoordinators = async () => {
-        let headers = {
-            "Content-Type": "application/json",
-            "Authorization": "JWT " + String(authTokens.access),
-            "Accept": "application/json"
-        }
-        let response = await fetch('/api/coordinator/', {headers: headers})
-        let data = await response.json()
-        setCoordinators(data)
-    };
 
     let getGroups = async () => {
         let headers = {
@@ -54,7 +28,7 @@ export const GroupsPage = () => {
     return (
         <Container>
             {groups?.map((group) => (
-                <GroupCard group={group} coordinators={coordinators} advisors={advisors}/>
+                <GroupCard group={group}/>
             ))}
         </Container>
 
