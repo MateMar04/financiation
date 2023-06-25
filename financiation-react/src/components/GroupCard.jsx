2670db5c141b0d82706168feb905fd38713d03ac
Mateo Marchisone <mateo.marchisone@gmail.com>
2023-06-25 12:10:43 -0300

    Fixed GroupCard

--- financiation-react/src/components/GroupCard.jsx
+++ financiation-react/src/components/GroupCard.jsx
@@ -1,39 +1,39 @@
 import React from 'react';
 import {Card, Col, Container, Row} from 'react-bootstrap';
 import "../assets/styles/GroupCard.css"
+import {UserParagraph} from "./UserParagraph";
 
 
-export const GroupCard = ({group, advisors, coordinators}) => {
+export const GroupCard = ({group, advisors, coordinator}) => {
     return (
         <Container fluid className='CompletlyContainer'>
             <Card className='groupcard'>
-            <Container fluid className='ContainerNameGroup'>
-                <h2 className='b'>Grupo numero {group.name}</h2>
+                <Container fluid className='ContainerNameGroup'>
+                    <h2 className='b'>{group.name}</h2>
                 </Container>
                 <Row className='Columna1'>
 
-                    <Col >
-                    <Container>
-                        <h3 className='a'>Asesores</h3>
+                    <Col>
+                        <Container>
+                            <h3 className='a'>Asesores</h3>
                         </Container>
                         {advisors?.map((advisor) => (
-                          <p>{advisor.id_user}</p>
+                            <UserParagraph userId={advisor.id_user}/>
                         ))}
+
                     </Col>
                     <Col>
-                    
-                    <Container>
-                        <h3 className='a'>Coordinadores</h3>
+
+                        <Container>
+                            <h3 className='a'>Coordinadores</h3>
                         </Container>
-                        {coordinators?.map  ((coordinator) => (
-                          <p>{coordinator.id_user}</p>
-                        ))}
+                        <UserParagraph userId={coordinator.id_user}/>
                     </Col>
                 </Row>
             </Card>
 
         </Container>
-        
+
     )
 }
 
