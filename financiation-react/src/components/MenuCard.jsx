e7063fe0112144ce410dcb0f086be367542e35e4
Mateo Marchisone <mateo.marchisone@gmail.com>
2023-06-25 21:33:28 -0300

    Margin and Padding

--- financiation-react/src/components/MenuCard.jsx
+++ financiation-react/src/components/MenuCard.jsx
@@ -7,12 +7,13 @@ export const MenuCard = ({title, button1, button2, link1, link2}) => {
         <Card className="menu-card">
             <Card.Header><h3>{title}</h3></Card.Header>
             <Card.Body className="menu-card-body">
-                <Row>
+                <Row className="menu-card-row">
                     <Link to={link1} className="menu-card-link">
                         <Button className="menu-card-button">{button1}</Button>
                     </Link>
                 </Row>
-                <Row>
+                <hr/>
+                <Row className="menu-card-row">
                     <Link to={link2} className="menu-card-link">
                         <Button className="menu-card-button">{button2}</Button>
                     </Link>
