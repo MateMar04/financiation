4136b3769d59accfed70eabb50c544fd0b862ac7
LautaroVelez <lautivelez28@gmail.com>
2023-06-29 10:46:42 -0300

    accordeon added :D

--- financiation-react/src/components/GroupCard.jsx
+++ financiation-react/src/components/GroupCard.jsx
@@ -3,6 +3,11 @@ import {Card, Col, Container, Row} from 'react-bootstrap';
 import "../assets/styles/GroupCard.css"
 import {UserParagraph} from "./UserParagraph";
 import AuthContext from "../context/AuthContext";
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 
 export const GroupCard = ({group}) => {
@@ -41,35 +46,52 @@ export const GroupCard = ({group}) => {
 
     return (
         <Container fluid className='CompletlyContainer'>
-            <Card className='groupcard'>
-                <Container fluid className='ContainerNameGroup'>
-                    <h2 className='b'>{group.name}</h2>
-                </Container>
-                <Row className='Columna1'>
+            <div>
+                <Accordion>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon/>}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
+                    >
+                        <Typography>{group.name}</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                            malesuada lacus ex, sit amet blandit leo lobortis eget.
+                        </Typography>
+                    </AccordionDetails>
+                </Accordion>
+                </div>
+                <Card className='groupcard'>
+                    <Container fluid className='ContainerNameGroup'>
+                        <h2 className='b'>{group.name}</h2>
+                    </Container>
+                    <Row className='Columna1'>
 
-                    <Col>
-                        <Container>
-                            <h3 className='a'>Asesores</h3>
-                        </Container>
-                        {advisors?.map((advisor) => (
-                            <UserParagraph userId={advisor.id_user}/>
-                        ))}
+                        <Col>
+                            <Container>
+                                <h3 className='a'>Asesores</h3>
+                            </Container>
+                            {advisors?.map((advisor) => (
+                                <UserParagraph userId={advisor.id_user}/>
+                            ))}
 
-                    </Col>
-                    <Col>
-                        <Container>
-                            <h3 className='a'>Coordinadores</h3>
-                        </Container>
-                        {coordinators?.map((coordinator) => (
-                            <UserParagraph userId={coordinator.id_user}/>
-                        ))}
-                    </Col>
-                </Row>
-            </Card>
+                        </Col>
+                        <Col>
+                            <Container>
+                                <h3 className='a'>Coordinadores</h3>
+                            </Container>
+                            {coordinators?.map((coordinator) => (
+                                <UserParagraph userId={coordinator.id_user}/>
+                            ))}
+                        </Col>
+                    </Row>
+                </Card>
 
         </Container>
 
-    )
+)
 }
 
 export default GroupCard;
