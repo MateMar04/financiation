700bbdeb1e7ce3b0bab4a1589add85d2d86c78c3
mateosinches <71389954+mateosinches@users.noreply.github.com>
2023-07-26 08:32:45 -0300

    lg cambiado

--- financiation-react/src/pages/MainMenuPage.jsx
+++ financiation-react/src/pages/MainMenuPage.jsx
@@ -6,23 +6,24 @@ export const MainMenuPage = () => {
     return (
         <Container fluid>
             <Row>
-                <Col lg={4}>
+                <Col lg={6}>
                     <MenuCard title="Visitas" button1="Ver Visitas" button2="Crear Visita" link1="/visits/"
                               link2="/visit/add/"/>
                 </Col>
-                <Col lg={4}>
+                <Col lg={6}>
                     <MenuCard title="Grupos" button1="Ver Grupos" button2="Crear Grupo" link1="/group/"
                               link2="/group/add/"/>
                 </Col>
-                <Col lg={4}>
-                    <MenuCard title="Asesores" button1="Ver Asesores" button2="Crear Asesor" link1="/advisors/"
-                              link2="/advisor/add/"/>
-                </Col>
+                
             </Row>
 
 
             <Row>
-                <Col lg={4}>
+                <Col lg={6}>
+                    <MenuCard title="Asesores" button1="Ver Asesores" button2="Crear Asesor" link1="/advisors/"
+                              link2="/advisor/add/"/>
+                </Col>
+                <Col lg={6}>
                     <MenuCard title="Coordinadores" button1="Ver Coordinadores" button2="Crear Coordinador"
                               link1="/coordinators/"
                               link2="/coordinator/add/"/>
