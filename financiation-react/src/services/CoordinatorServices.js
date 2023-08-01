87ecae5073dfdbf6114dd15f15d175f353d3164e
Mateo Marchisone <mateo.marchisone@gmail.com>
2023-08-01 15:27:08 -0300

    Coordinator Services

---
+++ financiation-react/src/services/CoordinatorServices.js
@@ -0,0 +1,11 @@
+export const getCoordinators = async (tokens) => {
+    let headers = {
+        "Content-Type": "application/json",
+        "Authorization": "JWT " + String(tokens),
+        "Accept": "application/json"
+    }
+    let response = await fetch('/api/coordinator/', {headers: headers})
+    let data = await response.json()
+    return data
+}
+
