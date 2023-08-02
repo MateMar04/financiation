61437b5e51b4a61dadf0ae7c4ec321dec548668d
LautaroVelez <lautivelez28@gmail.com>
2023-08-02 10:54:37 -0300

    side bar added

--- financiation-react/src/pages/CreateGroupPage.jsx
+++ financiation-react/src/pages/CreateGroupPage.jsx
@@ -1,11 +1,23 @@
 import React, {useContext, useEffect, useState} from "react";
-import {Button, Card, Col, Container, Form, Modal, Row} from "react-bootstrap";
+import {Card, Col, Container, Form, Modal, Row} from "react-bootstrap";
 import '../assets/styles/CreateGroupPage.css'
 import AuthContext from "../context/AuthContext";
 import {UserRowWithRadio} from "../components/UserRowWithRadio";
 import {UserRowWithCheck} from "../components/UserRowWithCheck";
 import {Link, useNavigate} from 'react-router-dom'
 import Check from "../assets/images/checked.gif";
+
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
 
 
 export const CreateGroupPage = () => {
@@ -64,37 +76,103 @@ export const CreateGroupPage = () => {
         }
     }
 
+    const [state, setState] = React.useState({
+        top: false,
+        left: false,
+        bottom: false,
+        right: false,
+    });
+
+    const toggleDrawer = (anchor, open) => (event) => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+
+        setState({...state, [anchor]: open});
+    };
+
+    const list = (anchor) => (
+        <Box
+            sx={{width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250}}
+            role="presentation"
+            onClick={toggleDrawer(anchor, false)}
+            onKeyDown={toggleDrawer(anchor, false)}
+        >
+            <List>
+                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                {index % 2 === 0 ? <InboxIcon/> : <MailIcon/>}
+                            </ListItemIcon>
+                            <ListItemText primary={text}/>
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+            <Divider/>
+            <List>
+                {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                {index % 2 === 0 ? <InboxIcon/> : <MailIcon/>}
+                            </ListItemIcon>
+                            <ListItemText primary={text}/>
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </Box>
+    );
+
     return (
         <Container fluid>
             <Form onSubmit={postGroup}>
-                <Card className='create-group-card'>
-                    <Row className='upper-row'>
-                        <Col>
-                            <h3>Nombre del Grupo</h3>
-                            <Form.Control name="name" placeholder='Nombre' type='text'></Form.Control>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col lg={6} className='create-group-column'>
-                            <h3>Coordinador</h3>
-                            <Container className='create-group-card-scroll'>
-                                {coordinators?.map((coordinator) => (
-                                    <UserRowWithRadio userId={coordinator.id_user}/>
-                                ))}
-                            </Container>
-                        </Col>
-                        <Col lg={6} className='create-group-column'>
-                            <h3>Asesores</h3>
-                            <Container className='create-group-card-scroll'>
-                                {advisors?.map((advisor) => (
-                                    <UserRowWithCheck userId={advisor.id_user}></UserRowWithCheck>
-                                ))}
-                            </Container>
-                        </Col>
-                    </Row>
-                    <Button type="submit">Crear</Button>
-                </Card>
+
+                <Row className='upper-row'>
+                    <Col>
+                        <Form.Control name="name" placeholder='Nombre' type='text'></Form.Control>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col lg={6} className='create-group-column'>
+                        <h3>Coordinador</h3>
+                        <Container className='create-group-card-scroll'>
+                            {coordinators?.map((coordinator) => (
+                                <UserRowWithRadio userId={coordinator.id_user}/>
+                            ))}
+                        </Container>
+                    </Col>
+                    <Col lg={6} className='create-group-column'>
+                        <h3>Asesores</h3>
+                        <Container className='create-group-card-scroll'>
+                            {advisors?.map((advisor) => (
+                                <UserRowWithCheck userId={advisor.id_user}></UserRowWithCheck>
+                            ))}
+                        </Container>
+                    </Col>
+                </Row>
+                <Button type="submit">Crear</Button>
+
             </Form>
+
+            <div>
+                {['right'].map((anchor) => (
+                    <React.Fragment key={anchor}>
+                        <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+                        <Drawer
+                            anchor={anchor}
+                            open={state[anchor]}
+                            onClose={toggleDrawer(anchor, false)}
+                        >
+                            {list(anchor)}
+                        </Drawer>
+                    </React.Fragment>
+                ))}
+            </div>
+
+
             <Modal show={show} onHide={handleClose}>
                 <Modal.Body>
                     <Container className='justify-content-center'>
