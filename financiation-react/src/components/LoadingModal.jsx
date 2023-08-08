38b5582fc2c8b1e560ad48337472c651009eb5fe
Julian Rosas <julymaxrosas@gmail.com>
2023-08-08 12:50:14 -0300

    avances

---
+++ financiation-react/src/components/LoadingModal.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import '../assets/styles/RowWithCheck.css'
+import Check from "../assets/images/Loading.gif";
+import {Link} from "react-router-dom";
+import {Button, Col, Container, Modal, Row} from "react-bootstrap";
+
+
+export const LoadingModal = (props, {message}) => {
+    return 
+            
+    
+}
+
+export default LoadingModal
