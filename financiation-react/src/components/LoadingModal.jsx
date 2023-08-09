89a06eaabf7f1b6e2a95e2dff33dfbf22d367ee1
Julian Rosas <julymaxrosas@gmail.com>
2023-08-09 10:14:08 -0300

    avances

--- financiation-react/src/components/LoadingModal.jsx
+++ financiation-react/src/components/LoadingModal.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import '../assets/styles/RowWithCheck.css'
-import Check from "../assets/images/Loading.gif";
+import Load from "../assets/images/Loading.gif";
 import {Link} from "react-router-dom";
 import {Button, Col, Container, Modal, Row} from "react-bootstrap";
 
 
 export const LoadingModal = (props, {message}) => {
-    return 
+    return (
+        <Modal show={props.show}>
+            <Modal.Body>
+                <Container className='justify-content-center'>
+                    <Row className='justify-content-center'>
+                        <Col lg={5}>
+                            <img src={Load} alt="CheckButton" className="mx-auto img-fluid"/>
+                        </Col>
+                    </Row>
+                </Container>
+            </Modal.Body>
+            <Modal.Footer>
+                    <Link to={'/'}>
+                    </Link>
+            </Modal.Footer>
+        </Modal>
+)
             
     
 }
