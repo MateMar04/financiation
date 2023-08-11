a134061117afeb05cb22e189e8ee4b5d3a1c1428
Mateo Marchisone <mateo.marchisone@gmail.com>
2023-08-11 20:15:06 -0300

    Simplifying code

--- financiation-react/src/services/ReportServices.js
+++ financiation-react/src/services/ReportServices.js
@@ -1,19 +1,19 @@
 export const dataHandler = (item) => {
     if (Object.keys(item).length === 3) {
         if (item.id_ministry_department !== undefined) {
-            faqsHandler(item)
+            toggle(faqs, item)
             console.log("IS FAQ")
         } else {
             if (item.id_department !== undefined) {
-                locationsHandler(item)
+                toggle(locations, item)
                 console.log("IS LOCATION")
             } else {
-                ministryDepartmentsHandler(item)
+                toggle(ministryDepartments, item)
                 console.log("IS MINISTRY")
             }
         }
     } else {
-        visitsHandler(item)
+        toggle(visits, item)
         console.log("IS VISIT")
     }
 }
@@ -24,43 +24,12 @@ const faqs = {}
 const ministryDepartments = {}
 const locations = {}
 
-const visitsHandler = (visit) => {
-    if (visit.id in visits) {
-        visits[visit.id] = visits[visit.id] + 1
-        console.log(visits)
-    } else {
-        visits[visit.id] = 1
-        console.log(visits)
-    }
 
-}
-
-const faqsHandler = (faq) => {
-    if (faq.id in faqs) {
-        faqs[faq.id] = faqs[faq.id] + 1
-        console.log(faqs)
+const toggle = (dict, item) => {
+    if (item.id in dict) {
+        delete dict[item.id]
     } else {
-        faqs[faq.id] = 1
-        console.log(faqs)
+        dict[item.id] = item.id
     }
-}
-
-const locationsHandler = (location) => {
-    if (location.id in locations) {
-        locations[location.id] = locations[location.id] + 1
-        console.log(locations)
-    } else {
-        locations[location.id] = 1
-        console.log(locations)
-    }
-}
-
-const ministryDepartmentsHandler = (ministryDepartment) => {
-    if (ministryDepartment.id in ministryDepartments) {
-        ministryDepartments[ministryDepartment.id] = ministryDepartments[ministryDepartment.id] + 1
-        console.log(ministryDepartments)
-    } else {
-        ministryDepartments[ministryDepartment.id] = 1
-        console.log(ministryDepartments)
-    }
+    console.log(dict)
 }
