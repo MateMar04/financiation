6fc3ba3a226e20b797a2d37dd994ee033aa9cef1
LautaroVelez <lautivelez28@gmail.com>
2023-08-14 00:38:15 -0300

    transitions to groupcard

--- financiation-react/src/components/GroupCard.jsx
+++ financiation-react/src/components/GroupCard.jsx
@@ -10,6 +10,8 @@ import {AdvisorMiniCardGroup} from "./AdvisorMiniCardGroup";
 import {CoordinatorMiniCardGroup} from "./CoordinatorMiniCardGroup";
 import CreateIcon from '@mui/icons-material/Create';
 import IconButton from "@mui/material/IconButton";
+import {Fade} from "@mui/material";
+
 
 
 export const GroupCard = ({group}) => {
@@ -30,37 +32,42 @@ export const GroupCard = ({group}) => {
 
     return (
         <Container fluid className='CompletlyContainer'>
-            <div>
-                <Accordion>
-                    <AccordionSummary
-                        expandIcon={<ExpandMoreIcon/>}
-                        aria-controls="panel1a-content"
-                        id="panel1a-header"
-                    >
-                        <Typography>{group.name}</Typography>
+            <Fade in>
 
-                    </AccordionSummary>
+                <div>
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon/>}
+                            aria-controls="panel1a-content"
+                            id="panel1a-header"
+                        >
+                            <Typography>{group.name}</Typography>
 
-                    <AccordionDetails>
-                        <IconButton type="submit" aria-label="search" onClick={handleAddButton}>
-                            <CreateIcon/>
-                        </IconButton>
-                        <Container>
-                            <Row>
-                                <Col>
-                                    <AdvisorMiniCardGroup group={group} showButton={showButton} DeleteAdvisor={handleDeleteAdvisor}/>
-                                </Col>
-                                <Col md={1}>
-                                    <div className="vl"></div>
-                                </Col>
-                                <Col>
-                                    <CoordinatorMiniCardGroup group={group} showButton={showButton} DeleteCoordinator={handleDeleteCoordinator}/>
-                                </Col>
-                            </Row>
-                        </Container>
-                    </AccordionDetails>
-                </Accordion>
-            </div>
+                        </AccordionSummary>
+
+                        <AccordionDetails>
+                            <IconButton type="submit" aria-label="search" onClick={handleAddButton}>
+                                <CreateIcon/>
+                            </IconButton>
+                            <Container>
+                                <Row>
+                                    <Col>
+                                        <AdvisorMiniCardGroup group={group} showButton={showButton}
+                                                              DeleteAdvisor={handleDeleteAdvisor}/>
+                                    </Col>
+                                    <Col md={1}>
+                                        <div className="vl"></div>
+                                    </Col>
+                                    <Col>
+                                        <CoordinatorMiniCardGroup group={group} showButton={showButton}
+                                                                  DeleteCoordinator={handleDeleteCoordinator}/>
+                                    </Col>
+                                </Row>
+                            </Container>
+                        </AccordionDetails>
+                    </Accordion>
+                </div>
+            </Fade>
         </Container>
 
     )
