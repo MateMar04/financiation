9a993b94206188c8984966691493ebd31a72b897
mateosinches <71389954+mateosinches@users.noreply.github.com>
2023-08-15 10:46:36 -0300

    cambios en el mainmenu

--- financiation-react/src/pages/MainMenuPage.jsx
+++ financiation-react/src/pages/MainMenuPage.jsx
@@ -1,51 +1,91 @@
-import {Col, Container, Row} from "react-bootstrap";
-import "../assets/styles/MainMenuPage.css"
-import {MenuCard} from "../components/MenuCard";
+import { Col, Container, Row, DropdownButton, Dropdown } from "react-bootstrap";
+import "../assets/styles/MainMenuPage.css";
+import { MenuCard } from "../components/MenuCard";
+import finance from "../assets/images/financepic.jpg";
+import visitas from "../assets/images/visitaspic.jpeg";
+import grupos from "../assets/images/grupospic.jpeg";
+import asesores from "../assets/images/asesorespic.jpeg";
+import coordinadores from "../assets/images/coordinadorespic.jpeg";
+import asesorados from "../assets/images/asesoradospic.jpeg";
+import intendentes from "../assets/images/intendentespic.jpeg";
 
 export const MainMenuPage = () => {
     return (
-        <Container fluid>
-            <Row>
-                <Col lg={3}>
-                <h1 className="tit">Main Menu </h1>
-                </Col>
-            </Row>
-            <Row>
-                <Col lg={6}>
-                    <MenuCard title="Visitas" button1="Ver Visitas" button2="Crear Visita" link1="/visits/"
-                              link2="/visit/add/"/>
-                </Col>
-            
-            
-                <Col lg={6}>
-                    <MenuCard title="Grupos" button1="Ver Grupos" button2="Crear Grupo" link1="/group/"
-                              link2="/group/add/"/>
-                </Col>
-            </Row>
-            <Row>
-                <Col lg={6}>
-                    <MenuCard title="Asesores" button1="Ver Asesores" button2="Crear Asesor" link1="/advisors/"
-                              link2="/advisor/add/"/>
-                </Col>
-          
-                <Col lg={6}>
-                    <MenuCard title="Coordinadores" button1="Ver Coordinadores" button2="Crear Coordinador"
-                              link1="/coordinators/"
-                              link2="/coordinator/add/"/>
-                </Col>
-<<<<<<< HEAD
-=======
-                <Col lg={4}>
-                    <MenuCard title="Asesorados" button1="Ver Asesorados" button2="Crear Asesorado" link1="/advised/"
-                              link2="/advised/add/"/>
-                </Col>
-                <Col lg={4}>
-                </Col>
-                <Col lg={4}>
-                </Col>
->>>>>>> e95ec53a64d20bbbe5b4305fbecfa43c70adc4cf
-            </Row>
+        <Container fluid className="menuback full-height">
+            <img src={finance} alt="" className="imagen" />
 
+            <Row className="justify-content-center">
+                <Col lg={3} md={6} className="cardd">
+                    <MenuCard
+                        title="Visitas"
+                        link1="/visits/"
+                        link2="/visit/add/"
+                        image={visitas}
+                        customText="Proporciona una vista general de todas las visitas programadas y realizadas."
+                        title1="Ver Visitas"
+                        title2="Crear Visitas"
+                    />
+                </Col>
+
+                <Col lg={3} md={6} className="cardd">
+                    <MenuCard
+                        title="Grupos"
+                        link1="/group/"
+                        link2="/group/add/"
+                        image={grupos}
+                        customText="Proporciona una vista general de todos los grupos existentes en tu sistema."
+                        title1="Ver Grupos"
+                        title2="Crear Grupos"
+                    />
+                </Col>
+
+                <Col lg={3} md={6} className="cardd">
+                    <MenuCard
+                        title="Asesores"
+                        link1="/advisors/"
+                        link2="/advisor/add/"
+                        image={asesores}
+                        customText="Permite explorar la lista y acceder a detalles específicos de cada asesor."
+                        title1="Ver Asesores"
+                        title2="Crear Asesores"
+                    />
+                </Col>
+            </Row>
+            <Row className="justify-content-center">
+                <Col lg={3} md={6} className="cardd">
+                    <MenuCard
+                        title="Coordinadores"
+                        link1="/coordinators/"
+                        link2="/coordinator/add/"
+                        image={coordinadores}
+                        customText="Proporciona una lista completa de todos los coordinadores registrados en tu plataforma."
+                        title1="Ver Coordinadores"
+                        title2="Crear Coordinadores"
+                    />
+                </Col>
+                <Col lg={3} md={6} className="cardd">
+                    <MenuCard
+                        title="Asesorados"
+                        link1="/advised/"
+                        link2="/advised/add/"
+                        image={asesorados}
+                        customText="Proporciona una lista completa de todas las personas que están siendo asesoradas en tu plataforma."
+                        title1="Ver Asesorados"
+                        title2="Crear Asesorados"
+                    />
+                </Col>
+                <Col lg={3} md={6} className="cardd">
+                    <MenuCard
+                        title="Intendentes"
+                        link1="/advised/"
+                        link2="/advised/add/"
+                        image={intendentes}
+                        customText="Proporciona una lista completa de todas las personas que están siendo asesoradas en tu plataforma."
+                        title1="Ver Intendentes"
+                        title2="Crear Intendentes"
+                    />
+                </Col>
+            </Row>
         </Container>
-    )
-}
+    );
+};
