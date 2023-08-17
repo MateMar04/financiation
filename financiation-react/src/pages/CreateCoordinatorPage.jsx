dececa50cfd9c64fecc82b279cf9130be42b5351
LautaroVelez <lautivelez28@gmail.com>
2023-08-17 11:12:56 -0300

    back to old version CreateAdvisorPage.jsx and CreateCoordinatorPage.jsx

--- financiation-react/src/pages/CreateCoordinatorPage.jsx
+++ financiation-react/src/pages/CreateCoordinatorPage.jsx
@@ -1,55 +1,51 @@
 import React, {useContext, useState} from "react";
 import {Button, Container, Form} from "react-bootstrap";
 import '../assets/styles/ActivateAccountPAge.css'
+import {useNavigate} from "react-router-dom";
 import AuthContext from "../context/AuthContext";
-import {SucceedModal} from "../components/SucceedModal"
-import FailedModal from "../components/FailedModal";
 
 const CoordinatorPage = () => {
     let {authTokens} = useContext(AuthContext)
-    const [showfail, setShowfailture] = useState(false);
-    const [showsuccess, setShowsuccese] = useState(false);
-    const toggleModalsucceed = () => setShowsuccese(!showsuccess);
-    const toggleModalfailed = () => setShowfailture(!showfail);
-
+    let history = useNavigate()
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
 
     let postCoordinator = async (e) => {
         e.preventDefault()
-        let response = await fetch('/api/coordinators', {
+        let response = await fetch('/api/coordinator/add/', {
             method: "POST",
             headers: {
                 'Content-Type': 'application/json',
                 "Authorization": "JWT " + String(authTokens.access),
-                "Accept": "application/json"
-            },
+                "Accept": "application/json"},
             body: JSON.stringify({
                 "id_user": e.target.id_user.value,
                 "id_group": e.target.id_group.value
             })
-        })
+    })
         if (response.status === 200) {
-            toggleModalsucceed();
             await postCoordinator()
-        } else if (response.status == 500) {
-            toggleModalfailed();
-            //alert('no se a registrado la visita (Uno de los datos ingresados no coincide con la base de datos)')
-            await postCoordinator()
-        } else if (response.status == 401) {
-            toggleModalfailed();
-            //alert('no se a registrado la visita (Desautorizado)')
-            await postCoordinator()
-        } else if (response.status == 400) {
-            toggleModalfailed();
-            //alert('no se a registrado la visita (Bad request)')
-            await postCoordinator()
+        } else if (response.status === 500) {
+            //handleShow()
+            //<SucceedModal message="la visita" onclose = {setShow(false)} show ={show}/>
+            //await postVisit()
+            alert('no se a registrado la visita (Hay un campo vacio)')
+        } else if (response.status === 401) {
+            //handleShow()
+            //<SucceedModal message="la visita" onclose = {setShow(false)} show ={show}/>
+            //await postVisit()
+            alert('no se a registrado la visita (Desautorizado)')
+        } else if (response.status === 400) {
+            //handleShow()
+            //<SucceedModal message="la visita" onclose = {setShow(false)} show ={show}/>
+            //await postVisit()
+            alert('no se a registrado la visita (Bad request)')
         }
     }
 
-
     return (
+
         <Container className="scrolling">
-            <SucceedModal message="el coordinador" show={showsuccess}/>
-            <FailedModal message="el coordinador" show={showfail}/>
             <Form onSubmit={postCoordinator}>
                 <Form.Group>
                     <Form.Control
@@ -71,9 +67,7 @@ const CoordinatorPage = () => {
                     <Button type="submit">Submit</Button>
                 </Form.Group>
             </Form>
-
         </Container>
     );
 }
-
 export default CoordinatorPage;
