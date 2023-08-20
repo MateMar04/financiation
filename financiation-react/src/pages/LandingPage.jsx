034507f7bbcb2c20d93153b8181b14ccc14ee9ac
LautaroVelez <lautivelez28@gmail.com>
2023-08-20 20:38:44 -0300

    landing 50%

--- financiation-react/src/pages/LandingPage.jsx
+++ financiation-react/src/pages/LandingPage.jsx
@@ -2,32 +2,34 @@ import React from "react";
 import '../assets/styles/LandingPage.css'
 import imagenPanal from "../assets/images/panal.jpg";
 import Logo from "../assets/images/LOGOGOBIERNO.png";
-import {Button, Card, Container} from "react-bootstrap";
+import {Button, Card, Container, Row} from "react-bootstrap";
 import {Link} from "react-router-dom";
+
 
 const LandingPage = () => {
     return (
-        <Container fluid className="background">
-            <Container fluid>
-                <img src={imagenPanal} alt="" className="imagen"/>
-            </Container>
-            <Card id="carta">
-                <Container>
-                    <img src={Logo} alt="React Logo" className="img-fluid"/>
-                </Container>
-                <hr/>
-                <Container>
+        <div className={'stopScrolling'}>
+        <Container fluid className="containerLanding">
+                <div className="imagen">
+                <img src={imagenPanal} alt="" className="imagen-diagonal"/>
+                    </div>
+
+
+            <Container>
+                <Row className={'justify-content-end text-center'}>
+
+
+                    <img src={Logo} className="ImgLogoFinanzasLanding"/>
                     <p>Si ya tienes una cuenta puedes iniciar sesion</p>
                     <Link to='/login/'><Button>Iniciar Sesion</Button></Link>
-                </Container>
-                <hr/>
-                <Container>
                     <p>Si no tienes una cuenta puede crearte una</p>
                     <Link to='/signin/'><Button>Crea una Cuenta</Button></Link>
-                    <div className='py-2'></div>
-                </Container>
-            </Card>
+
+
+                </Row>
+            </Container>
         </Container>
+            </div>
     );
 }
 
