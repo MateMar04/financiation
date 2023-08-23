2b05745214252daab7171e516a90ba11f98ed6b6
LautaroVelez <lautivelez28@gmail.com>
2023-08-23 09:43:03 -0300

    userservices arreglado

--- financiation-react/src/services/UserServices.js
+++ financiation-react/src/services/UserServices.js
@@ -11,18 +11,6 @@ export const getUser = async (tokens) => {
     return data
 }
 
-export const getUsers = async (tokens) => {
-
-    let headers = {
-        "Content-Type": "application/json",
-        "Authorization": "JWT " + String(tokens),
-        "Accept": "application/json"
-    }
-
-    let response = await fetch(`/api/users`, {headers: headers})
-    let data = await response.json()
-    return data
-}
 
 export const getUserById = async (tokens, userId) => {
     let headers = {
