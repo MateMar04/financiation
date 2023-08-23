bc48156d4e99fc56e1615138f825e7c0aa85dc0b
mateosinches <71389954+mateosinches@users.noreply.github.com>
2023-08-23 12:25:47 -0300

    jsx xhanges

--- financiation-react/src/pages/SigninPage.jsx
+++ financiation-react/src/pages/SigninPage.jsx
@@ -1,85 +1,90 @@
-import React, {useContext} from "react";
-import '../assets/styles/LoginPage.css'
+import React, { useContext } from "react";
+import '../assets/styles/SigninPage.css'
 import Logo from "../assets/images/PRUEBA.PNG";
-import {Button, Col, Container, FloatingLabel, Form, Row} from "react-bootstrap";
-import {Link} from "react-router-dom";
+import { Button, Col, Container, FloatingLabel, Form, Row, } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Card from '@mui/material/Card';
 import AuthContext from "../context/AuthContext";
+import { MenuCard } from "../components/MenuCard";
 
 const SigninPage = () => {
-    let {signIn} = useContext(AuthContext)
+    let { signIn } = useContext(AuthContext)
     return (
         <Container fluid className="general">
-            <img src={Logo} alt="Logo ministerio de finanzas"/>
-            <Container>
-                <Form onSubmit={signIn}>
-                    <Row className='justify-content-md-center'>
-                        <Col lg="5" className='input-required'>
-                            <FloatingLabel className='floatingLabel' label="Nombre">
-                                <Form.Control placeholder="Nombre" type="text" className='input-required'
-                                              name='first_name' required/>
-                            </FloatingLabel>
-                        </Col>
-                        <Col lg="5" className='input-required'>
-                            <FloatingLabel className='floatingLabel' label="Apellido">
-                                <Form.Control placeholder="Apellido" type="text" name='last_name' required/>
-                            </FloatingLabel>
-                        </Col>
-                    </Row>
-
-                    <Row className='justify-content-md-center'>
-                        <Col lg="5" className='input-required'>
-                            <FloatingLabel className='floatingLabel' label="Usuario">
-                                <Form.Control placeholder="Usuario" type="text" name='username' required/>
-                            </FloatingLabel>
-                        </Col>
-                        <Col lg="5" className='input-required'>
-                            <FloatingLabel className='floatingLabel' label="DNI">
-                                <Form.Control placeholder="DNI" type="number" name='ssn' required/>
-                            </FloatingLabel>
-                        </Col>
-                    </Row>
-
-                    <Row className='justify-content-md-center'>
-                        <Col lg="5" className='input-required'>
-                            <FloatingLabel className='floatingLabel' label="Correo Electrónico">
-                                <Form.Control placeholder="Correo electronico" type="email" name='email' required/>
-                            </FloatingLabel>
-                        </Col>
-                        <Col lg="5" className='input-required'>
-                            <FloatingLabel className='floatingLabel' label="Telefono">
-                                <Form.Control placeholder="Telefono" type="tel" name='phone' required/>
-                            </FloatingLabel>
-                        </Col>
-                    </Row>
-
-                    <Row className='justify-content-md-center'>
-                        <Col lg="5" className='input-required'>
-                            <FloatingLabel className='floatingLabel' label="Contraseña">
-                                <Form.Control placeholder="Contraseña" type="password" name="password" required/>
-                            </FloatingLabel>
-                        </Col>
-                        <Col lg="5" className='input-required'>
-                            <FloatingLabel className='floatingLabel' label="Repita la contraseña">
-                                <Form.Control placeholder="Repita Contraseña" type="password" name="re_password"
-                                              required/>
-                            </FloatingLabel>
-                        </Col>
-                    </Row>
-
-                    <Container>
-                        <Row>
-                            <Col>
-                                <Button className="create" type="submit">Crear Cuenta</Button>
+            <Card className="cardsign">
+                <img src={Logo} className="logosign" alt="Logo ministerio de finanzas"/>
+                <Container>
+                    <h2>Crear Cuenta</h2>
+                    <Form onSubmit={signIn}>
+                        <Row className='justify-content-md-center'>
+                            <Col lg="5" className='input-required'>
+                                <FloatingLabel className='floatingLabel' label="Nombre">
+                                    <Form.Control placeholder="Nombre" type="text" className='input-required'
+                                        name='first_name' required />
+                                </FloatingLabel>
+                            </Col>
+                            <Col lg="5" className='input-required'>
+                                <FloatingLabel className='floatingLabel' label="Apellido">
+                                    <Form.Control placeholder="Apellido" type="text" name='last_name' required />
+                                </FloatingLabel>
                             </Col>
                         </Row>
-                        <Row>
-                            <Link to="/login/"><Button variant="link" className="link">Ya tengo una
-                                cuenta</Button></Link>
-                        </Row>
-                    </Container>
 
-                </Form>
-            </Container>
+                        <Row className='justify-content-md-center'>
+                            <Col lg="5" className='input-required'>
+                                <FloatingLabel className='floatingLabel' label="Usuario">
+                                    <Form.Control placeholder="Usuario" type="text" name='username' required />
+                                </FloatingLabel>
+                            </Col>
+                            <Col lg="5" className='input-required'>
+                                <FloatingLabel className='floatingLabel' label="DNI">
+                                    <Form.Control placeholder="DNI" type="number" name='ssn' required />
+                                </FloatingLabel>
+                            </Col>
+                        </Row>
+
+                        <Row className='justify-content-md-center'>
+                            <Col lg="5" className='input-required'>
+                                <FloatingLabel className='floatingLabel' label="Correo Electrónico">
+                                    <Form.Control placeholder="Correo electronico" type="email" name='email' required />
+                                </FloatingLabel>
+                            </Col>
+                            <Col lg="5" className='input-required'>
+                                <FloatingLabel className='floatingLabel' label="Telefono">
+                                    <Form.Control placeholder="Telefono" type="tel" name='phone' required />
+                                </FloatingLabel>
+                            </Col>
+                        </Row>
+
+                        <Row className='justify-content-md-center'>
+                            <Col lg="5" className='input-required'>
+                                <FloatingLabel className='floatingLabel' label="Contraseña">
+                                    <Form.Control placeholder="Contraseña" type="password" name="password" required />
+                                </FloatingLabel>
+                            </Col>
+                            <Col lg="5" className='input-required'>
+                                <FloatingLabel className='floatingLabel' label="Repita la contraseña">
+                                    <Form.Control placeholder="Repita Contraseña" type="password" name="re_password"
+                                        required />
+                                </FloatingLabel>
+                            </Col>
+                        </Row>
+
+                        <Container>
+                            <Row>
+                                <Col>
+                                    <Button className="create" id="botonsign" type="submit">Crear Cuenta</Button>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Link to="/login/"><Button variant="link" className="link">Ya tengo una
+                                    cuenta</Button></Link>
+                            </Row>
+                        </Container>
+
+                    </Form>
+                </Container>
+            </Card>
         </Container>
     );
 }
