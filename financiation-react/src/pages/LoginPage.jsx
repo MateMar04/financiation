6481b5d1abb24182127059d67facb8ea4fe3ff63
tadeofunes <71388800+tadeofunes@users.noreply.github.com>
2023-08-24 10:44:44 -0300

    FirstVersionFInished

--- financiation-react/src/pages/LoginPage.jsx
+++ financiation-react/src/pages/LoginPage.jsx
@@ -4,37 +4,71 @@ import Logo from "../assets/images/PRUEBA.PNG";
 import {Button, Card, Container, FloatingLabel, Form} from "react-bootstrap";
 import {Link} from "react-router-dom";
 import AuthContext from "../context/AuthContext";
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import PersonIcon from '@mui/icons-material/Person';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import PhoneIcon from '@mui/icons-material/Phone';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import LockIcon from '@mui/icons-material/Lock';
 
 const LoginPage = () => {
     let {loginUser} = useContext(AuthContext)
     return (
+        
+        
+    <Container fluid className="general">
         <Card className="GeneralCard">
-        <Container fluid className="general">
-            <Container fluid className="image-container">
-                <img src={Logo} alt="Logo del ministerio de finanzas"/>
-            </Container>
+            
+                <img src={Logo} className="Image"   alt="Logo del ministerio de finanzas"/>
+
             <Container className="Titulo1">
                     <h1>Iniciar Sesion</h1>
                 </Container>
             <Form onSubmit={loginUser}>
                 <Container>
-                    <FloatingLabel className='floatingLabel' label="Usuario">
-                        <Form.Control placeholder="Usuario" type="text" name="username" required/>
-                    </FloatingLabel>
+                <TextField
+                                    label="CUIL"
+                                    variant="standard"
+                                    InputProps={{
+                                        startAdornment: (
+                                            <InputAdornment position="start">
+                                                <AccountBoxIcon />
+                                            </InputAdornment>
+                                        ),
+                                    }}
+                                    type="text" // Agrega el tipo "text"
+                                    name='cuil'
+                                    required
+                                />
                 </Container>
                 <Container>
-                    <FloatingLabel className='floatingLabel' label="Contraseña">
-                        <Form.Control placeholder="Contraseña" type="password" name="password" minLength='6' required/>
-                    </FloatingLabel>
+                
+                <TextField
+                                    label="Contraseña"
+                                    variant="standard"
+                                    InputProps={{
+                                        startAdornment: (
+                                            <InputAdornment position="start">
+                                                <LockIcon />
+                                            </InputAdornment>
+                                        ),
+                                    }}
+                                    type="password"
+                                    name="password"
+                                    required
+                                />
                 </Container>
                 <Container fluid>
-                    <Button type="submit">Acceder</Button>
+                    <Button type="submit" className="AccesButton">Acceder</Button>
                 </Container>
             </Form>
-            <Link to="/signin/"><Button variant="link" className="link">No tengo una cuenta</Button></Link>
             <Link to="/reset-password/"><Button variant="link" className="link">Me olvidé la contraseña</Button></Link>
+            <Link to="/signin/"><Button variant="link" className="link">No tengo una cuenta</Button></Link>
+            
+            </Card>
         </Container>
-        </Card>
+        
     )
 };
 
