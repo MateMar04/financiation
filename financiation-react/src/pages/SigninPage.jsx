c081703683e70074d5303f00ef0bf99290ebeb96
mateosinches <71389954+mateosinches@users.noreply.github.com>
2023-08-24 09:55:04 -0300

    code changes

--- financiation-react/src/pages/SigninPage.jsx
+++ financiation-react/src/pages/SigninPage.jsx
@@ -1,84 +1,180 @@
 import React, { useContext } from "react";
 import '../assets/styles/SigninPage.css'
 import Logo from "../assets/images/PRUEBA.PNG";
-import { Button, Col, Container, FloatingLabel, Form, Row, } from "react-bootstrap";
+import { Col, Container, FloatingLabel, Form, Row, } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Card from '@mui/material/Card';
 import AuthContext from "../context/AuthContext";
-import { MenuCard } from "../components/MenuCard";
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import PersonIcon from '@mui/icons-material/Person';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import PhoneIcon from '@mui/icons-material/Phone';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import LockIcon from '@mui/icons-material/Lock';
+import Button from '@mui/material/Button';
+
+
 
 const SigninPage = () => {
     let { signIn } = useContext(AuthContext)
     return (
         <Container fluid className="general">
             <Card className="cardsign">
-                <img src={Logo} className="logosign" alt="Logo ministerio de finanzas"/>
+                <img src={Logo} className="logosign" alt="Logo ministerio de finanzas" />
                 <Container>
                     <h2>Crear Cuenta</h2>
                     <Form onSubmit={signIn}>
                         <Row className='justify-content-md-center'>
                             <Col lg="5" className='input-required'>
-                                <FloatingLabel className='floatingLabel' label="Nombre">
-                                    <Form.Control placeholder="Nombre" type="text" className='input-required'
-                                        name='first_name' required />
-                                </FloatingLabel>
+                                <TextField
+                                    label="Apellido"
+                                    variant="standard"
+                                    InputProps={{
+                                        startAdornment: (
+                                            <InputAdornment position="start">
+                                                <PersonIcon />
+                                            </InputAdornment>
+                                        ),
+                                    }}
+                                    name='last_name'
+                                    required
+                                />
                             </Col>
                             <Col lg="5" className='input-required'>
-                                <FloatingLabel className='floatingLabel' label="Apellido">
-                                    <Form.Control placeholder="Apellido" type="text" name='last_name' required />
-                                </FloatingLabel>
-                            </Col>
-                        </Row>
-
-                        <Row className='justify-content-md-center'>
-                            <Col lg="5" className='input-required'>
-                                <FloatingLabel className='floatingLabel' label="Usuario">
-                                    <Form.Control placeholder="Usuario" type="text" name='username' required />
-                                </FloatingLabel>
-                            </Col>
-                            <Col lg="5" className='input-required'>
-                                <FloatingLabel className='floatingLabel' label="DNI">
-                                    <Form.Control placeholder="DNI" type="number" name='ssn' required />
-                                </FloatingLabel>
-                            </Col>
-                        </Row>
-
-                        <Row className='justify-content-md-center'>
-                            <Col lg="5" className='input-required'>
-                                <FloatingLabel className='floatingLabel' label="Correo Electrónico">
-                                    <Form.Control placeholder="Correo electronico" type="email" name='email' required />
-                                </FloatingLabel>
-                            </Col>
-                            <Col lg="5" className='input-required'>
-                                <FloatingLabel className='floatingLabel' label="Telefono">
-                                    <Form.Control placeholder="Telefono" type="tel" name='phone' required />
-                                </FloatingLabel>
+                                <TextField
+                                    label="Apellido"
+                                    variant="standard"
+                                    InputProps={{
+                                        startAdornment: (
+                                            <InputAdornment position="start">
+                                                <PersonIcon />
+                                            </InputAdornment>
+                                        ),
+                                    }}
+                                    name='last_name'
+                                    required
+                                />
                             </Col>
                         </Row>
 
                         <Row className='justify-content-md-center'>
                             <Col lg="5" className='input-required'>
-                                <FloatingLabel className='floatingLabel' label="Contraseña">
-                                    <Form.Control placeholder="Contraseña" type="password" name="password" required />
-                                </FloatingLabel>
+                                <TextField
+                                    label="Teléfono"
+                                    variant="standard"
+                                    InputProps={{
+                                        startAdornment: (
+                                            <InputAdornment position="start">
+                                                <PhoneIcon />
+                                            </InputAdornment>
+                                        ),
+                                    }}
+                                    type="tel"
+                                    name="phone"
+                                    required
+                                />
                             </Col>
                             <Col lg="5" className='input-required'>
-                                <FloatingLabel className='floatingLabel' label="Repita la contraseña">
-                                    <Form.Control placeholder="Repita Contraseña" type="password" name="re_password"
-                                        required />
-                                </FloatingLabel>
+                                <TextField
+                                    label="CUIL"
+                                    variant="standard"
+                                    InputProps={{
+                                        startAdornment: (
+                                            <InputAdornment position="start">
+                                                <AccountBoxIcon />
+                                            </InputAdornment>
+                                        ),
+                                    }}
+                                    type="text" // Agrega el tipo "text"
+                                    name='cuil'
+                                    required
+                                />
+                            </Col>
+                        </Row>
+
+                        <Row className='justify-content-md-center'>
+                            <Col lg="5" className='input-required'>
+                                <TextField
+                                    label="Correo Electrónico"
+                                    variant="standard"
+                                    InputProps={{
+                                        startAdornment: (
+                                            <InputAdornment position="start">
+                                                <MailOutlineIcon />
+                                            </InputAdornment>
+                                        ),
+                                    }}
+                                    type="email"
+                                    name="email"
+                                    required
+                                />
+                            </Col>
+                            <Col lg="5" className='input-required'>
+                                <TextField
+                                    label="Repetir Correo Electrónico"
+                                    variant="standard"
+                                    InputProps={{
+                                        startAdornment: (
+                                            <InputAdornment position="start">
+                                                <MailOutlineIcon />
+                                            </InputAdornment>
+                                        ),
+                                    }}
+                                    type="email" // Usamos el tipo "email" para validar el formato
+                                    name='email'
+                                    required
+                                />
+                            </Col>
+                        </Row>
+
+                        <Row className='justify-content-md-center'>
+                            <Col lg="5" className='input-required'>
+                                <TextField
+                                    label="Contraseña"
+                                    variant="standard"
+                                    InputProps={{
+                                        startAdornment: (
+                                            <InputAdornment position="start">
+                                                <LockIcon />
+                                            </InputAdornment>
+                                        ),
+                                    }}
+                                    type="password"
+                                    name="password"
+                                    required
+                                />
+                            </Col>
+                            <Col lg="5" className='input-required'>
+                                <TextField
+                                    label="Repita la contraseña"
+                                    variant="standard"
+                                    InputProps={{
+                                        startAdornment: (
+                                            <InputAdornment position="start">
+                                                <LockIcon />
+                                            </InputAdornment>
+                                        ),
+                                    }}
+                                    type="password"
+                                    name="re_password"
+                                    required
+                                />
                             </Col>
                         </Row>
 
                         <Container>
                             <Row>
                                 <Col>
-                                    <Button className="create" id="botonsign" type="submit">Crear Cuenta</Button>
+                                    <Button variant="contained" id="botonsign" color="primary" type="submit">
+                                        Crear Cuenta
+                                    </Button>
                                 </Col>
                             </Row>
                             <Row>
-                                <Link to="/login/"><Button variant="link" className="link">Ya tengo una
-                                    cuenta</Button></Link>
+                                <Link to="/login/"><Button variant="link" className="link">
+                                    Ya tengo una cuenta
+                                </Button></Link>
                             </Row>
                         </Container>
 
