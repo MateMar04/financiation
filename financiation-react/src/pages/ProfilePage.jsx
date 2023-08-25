f09cfa7edf9aae3fd75d27abb8ee26dc4537b860
LautaroVelez <lautivelez28@gmail.com>
2023-08-25 12:54:23 -0300

    profile front done!

--- financiation-react/src/pages/ProfilePage.jsx
+++ financiation-react/src/pages/ProfilePage.jsx
@@ -12,6 +12,7 @@ import { DateField } from '@mui/x-date-pickers/DateField';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import EditIcon from '@mui/icons-material/Edit';
+import IconButton from '@mui/material/IconButton';
 
 
 
@@ -26,71 +27,96 @@ const ProfilePage = () => {
         getUser(authTokens.access).then(data => setUser(data))
     })
 
+    const [showButton, setShowButton] = useState(false);
+    const [showLogoutButton, setShowLogoutButton] = useState(true); // Nueva variable
+
+
+
+    const handleAddButton = () => {
+        setShowButton(!showButton);
+        setShowLogoutButton(!showLogoutButton); // Cambiar la visibilidad del botón de Cerrar Sesión
+
+    };
+
 
     return (
-
         <Container className="ContainerProfilePage">
-            <EditIcon color='action' className="EditIconProfile" sx={{width: 25, height:25} } />
+            <IconButton className="EditIconProfile" onClick={handleAddButton}><EditIcon color='action' sx={{ width: 25, height: 25 }} /></IconButton>
             <Row>
                 <Col className="d-flex justify-content-center">
                     <Avatar src={user?.profile_picture} sx={{ width: 200, height: 200 }} className="ProfilePicture" />
                 </Col>
-                
+
             </Row>
-            
+
 
             <Row className={'justify-content-center text-center'}>
                 <h1 className="ProfileText">{user?.first_name} {user?.last_name}</h1>
                 <h3 className="ProfileText">Coordinador</h3>
             </Row>
 
-        <Container className="InputsProfile">
+            <Container className="InputsProfile">
 
-            <Row className={"d-flex justify-content-center text-center"}>
-                <Col md={6} className="py-3">
+                <Row className={"d-flex justify-content-center text-center"}>
+                    <Col md={6} className="py-3">
 
-                    <TextField variant='outlined' label='Nombre' required className='profileTextField' defaultValue={user?.first_name} InputProps={{ sx: { borderRadius: 5} }}></TextField>
+                        <TextField variant='outlined' label='Nombre' required className='profileTextField' defaultValue={user?.first_name} InputProps={{ sx: { borderRadius: 5 } }}></TextField>
 
-                </Col>
-                <Col md={6} className="py-3">
+                    </Col>
+                    <Col md={6} className="py-3">
 
-                    <TextField variant='outlined' label='Apellido' required className='profileTextField' defaultValue={user?.last_name} InputProps={{ sx: { borderRadius: 5 } }}></TextField>
+                        <TextField variant='outlined' label='Apellido' required className='profileTextField' defaultValue={user?.last_name} InputProps={{ sx: { borderRadius: 5 } }}></TextField>
 
-                </Col>
-            </Row>
-
-
-            <Row className={"d-flex justify-content-center text-center"}>
-                <Col md={6} className="py-3">
-
-                    <TextField variant='outlined' label='CUIL' required className='profileTextField' defaultValue={user?.ssn} InputProps={{ sx: { borderRadius: 5 } }}></TextField>
-
-                </Col>
-                <Col md={6} className="py-3">
-
-                    <TextField variant='outlined' label='Telefono' required className='profileTextField' defaultValue={user?.phone_number} InputProps={{ sx: { borderRadius: 5 } }}></TextField>
-
-                </Col>
-            </Row>
-            <Row className={"d-flex justify-content-center text-center"}>
-                <Col md={6} xs={12} className="py-3">
-                    <LocalizationProvider dateAdapter={AdapterDayjs}>
-                        <DateField label="Fecha de Nacimiento" className='profileTextField' InputProps={{ sx: { borderRadius: 5 } }} variant="outlined" />
-                    </LocalizationProvider>
+                    </Col>
+                </Row>
 
 
-                </Col>
-                <Col md={6} className="py-3">
+                <Row className={"d-flex justify-content-center text-center"}>
+                    <Col md={6} className="py-3">
 
-                    <TextField variant='outlined' label='Ciudad' className='profileTextField' InputProps={{ sx: { borderRadius: 5 } }}></TextField>
+                        <TextField variant='outlined' label='CUIL' required className='profileTextField' defaultValue={user?.ssn} InputProps={{ sx: { borderRadius: 5 } }}></TextField>
 
-                </Col>
-            </Row>
-            <Row>
-                <Col className="d-flex justify-content-center py-3">
-                    <Button className='BtnProfileCerrarSesion' onClick={logoutUser} sx={{my:3}}>Cerrar Sesion</Button>
-                </Col>
-            </Row>
+                    </Col>
+                    <Col md={6} className="py-3">
+
+                        <TextField variant='outlined' label='Telefono' required className='profileTextField' defaultValue={user?.phone_number} InputProps={{ sx: { borderRadius: 5 } }}></TextField>
+
+                    </Col>
+                </Row>
+                <Row className={"d-flex justify-content-center text-center"}>
+                    <Col md={6} xs={12} className="py-3">
+                        <LocalizationProvider dateAdapter={AdapterDayjs}>
+                            <DateField label="Fecha de Nacimiento" className='profileTextField' InputProps={{ sx: { borderRadius: 5 } }} variant="outlined" />
+                        </LocalizationProvider>
+
+
+                    </Col>
+                    <Col md={6} className="py-3">
+
+                        <TextField variant='outlined' label='Ciudad' className='profileTextField' InputProps={{ sx: { borderRadius: 5 } }}></TextField>
+
+                    </Col>
+                </Row>
+                <Row>
+
+                    {showButton &&(
+                        <Col className="d-flex justify-content-center py-3">
+                            <Button className="CancelarBtnProfile" onClick={handleAddButton}>Cancelar</Button>
+                        </Col>
+                    )}
+
+                    {showLogoutButton && (
+                        <Col className="d-flex justify-content-center py-3">
+                            <Button className='BtnProfileCerrarSesion' onClick={logoutUser} sx={{ my: 3 }}>Cerrar Sesion</Button>
+                        </Col>
+                    )}
+
+                    {showButton &&(
+                        <Col className="d-flex justify-content-center py-3">
+                            <Button className="GuardarBtnProfile">Guardar</Button>
+                        </Col>
+                    )}
+                </Row>
             </Container>
         </Container >
 
