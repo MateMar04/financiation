3ca81e232d06840be5dae74f68c37316770c6276
LautaroVelez <lautivelez28@gmail.com>
2023-08-26 16:30:12 -0300

    done!

--- financiation-react/src/pages/LoginPage.jsx
+++ financiation-react/src/pages/LoginPage.jsx
@@ -1,7 +1,7 @@
 import React, {useContext} from "react";
 import '../assets/styles/LoginPage.css'
 import Logo from "../assets/images/PRUEBA.PNG";
-import {Button, Row, Container, Form} from "react-bootstrap";
+import {Button, Row, Container, Form, FloatingLabel} from "react-bootstrap";
 import {Link} from "react-router-dom";
 import AuthContext from "../context/AuthContext";
 import TextField from '@mui/material/TextField';
@@ -14,47 +14,66 @@ const LoginPage = () => {
     return (
 
         <Container className="ContainerLoginPage">
+
+            <Row className={'justify-content-center text-center'}>
+                <img src={Logo} className="ImgLogoLogin" alt="Logo del ministerio de finanzas"/>
+            </Row>
+
+            <Row className={'justify-content-center text-center'}>
+                <h3 className={'h3LoginPage'}>Iniciar Sesion</h3>
+            </Row>
+
             <Form onSubmit={loginUser}>
-                <Row className={'justify-content-center text-center'}>
-                    <img src={Logo} className="ImgLogoLogin" alt="Logo del ministerio de finanzas"/>
-                </Row>
-                <Row className={'justify-content-center text-center'}>
-                    <h3 className={'h3LoginPage'}>Iniciar Sesion</h3>
-                </Row>
 
                 <Row className={'justify-content-center text-center'}>
-                    <TextField
-                        label="CUIL"
-                        variant="outlined"
-                        InputProps={{ startAdornment: (<InputAdornment position="start"> <AccountCircleOutlinedIcon/> </InputAdornment> ),sx: { borderRadius: 6 , borderColor: '#f4f4f4'} }}
-                        type="number"
-                        name='ssn'
-                        required
-                        className={'InputLoginPage'}
-                    />
+                    <Form.Group>
+                        <TextField
+                            label="CUIL"
+                            variant="outlined"
+                            InputProps={{
+                                startAdornment: (
+                                    <InputAdornment position="start"> <AccountCircleOutlinedIcon/> </InputAdornment>),
+                                sx: {borderRadius: 6, borderColor: '#f4f4f4'}
+                            }}
+                            type="text"
+                            name='username'
+                            required
+                            className={'InputLoginPage'}
+                        />
+                    </Form.Group>
                 </Row>
+
                 <Row className={'justify-content-center text-center py-4'}>
-                    <TextField label="Contraseña" variant="outlined" InputProps={{ startAdornment: (<InputAdornment position="start"> <VpnKeyOutlinedIcon/> </InputAdornment> ),sx: { borderRadius: 6 , borderColor: '#f4f4f4'} }} type="password" name="password" required className={'InputLoginPage'} />
+                    <Form.Group>
+                        <TextField label="Contraseña" variant="outlined" InputProps={{
+                            startAdornment: (<InputAdornment position="start"> <VpnKeyOutlinedIcon/> </InputAdornment>),
+                            sx: {borderRadius: 6, borderColor: '#f4f4f4'}
+                        }} type="password" name="password" required className={'InputLoginPage'}/>
+                    </Form.Group>
                 </Row>
+
                 <Row className={'justify-content-center text-center'}>
                     <Button type="submit" className="BtnIniciarSesionLogin">Iniciar Sesion</Button>
                 </Row>
-
-                <Row className={'justify-content-center text-center'}>
-                    <Link to="/reset-password/">
-                        <p className={'pLoginPage'}>Olvidé mi contraseña</p>
-                    </Link>
-                </Row>
-                <Row className={'justify-content-center text-center'}>
-                    <Link to="/signin/">
-                        <p className={'SecondpLoginPage'}>Crea una cuenta</p>
-                    </Link>
-                </Row>
-
             </Form>
+
+            <Row className={'justify-content-center text-center'}>
+                <Link to="/reset-password/">
+                    <p className={'pLoginPage'}>Olvidé mi contraseña</p>
+                </Link>
+            </Row>
+            <Row className={'justify-content-center text-center'}>
+                <Link to="/signin/">
+                    <p className={'SecondpLoginPage'}>Crea una cuenta</p>
+                </Link>
+            </Row>
+
+
         </Container>
 
     )
 };
 
 export default LoginPage;
+
+
