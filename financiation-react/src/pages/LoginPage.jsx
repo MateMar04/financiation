916676f8fa46e73442cff6f3dc97f32b98fb8ee4
LautaroVelez <lautivelez28@gmail.com>
2023-08-26 12:54:40 -0300

    bettered

--- financiation-react/src/pages/LoginPage.jsx
+++ financiation-react/src/pages/LoginPage.jsx
@@ -1,7 +1,7 @@
 import React, {useContext} from "react";
 import '../assets/styles/LoginPage.css'
 import Logo from "../assets/images/PRUEBA.PNG";
-import {Button, Card, Container, FloatingLabel, Form} from "react-bootstrap";
+import {Button, Row, Container, Form} from "react-bootstrap";
 import {Link} from "react-router-dom";
 import AuthContext from "../context/AuthContext";
 import TextField from '@mui/material/TextField';
@@ -12,60 +12,58 @@ import LockIcon from '@mui/icons-material/Lock';
 const LoginPage = () => {
     let {loginUser} = useContext(AuthContext)
     return (
-        
-        
-    <Container fluid className="general">
-        <Card className="GeneralCard">
-            
-                <img src={Logo} className="Image"   alt="Logo del ministerio de finanzas"/>
 
-            <Container fluid className="Titulo1">
-                    <p>Iniciar Sesion</p>
-                </Container>
+        <Container className="ContainerLoginPage">
+            <Row className={'justify-content-center text-center'}>
+                <img src={Logo} className="ImgLogoLogin" alt="Logo del ministerio de finanzas"/>
+            </Row>
+            <Row className={'justify-content-center text-center'}>
+                <h3>Iniciar Sesion</h3>
+            </Row>
+
             <Form onSubmit={loginUser}>
-                
                 <TextField
-                                    label="CUIL"
-                                    variant="standard"
-                                    InputProps={{
-                                        startAdornment: (
-                                            <InputAdornment position="start">
-                                                <AccountBoxIcon />
-                                            </InputAdornment>
-                                        ),
-                                    }}
-                                    type="text" // Agrega el tipo "text"
-                                    name='cuil'
-                                    required
-                                />
-                
+                    label="CUIL"
+                    variant="standard"
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <AccountBoxIcon/>
+                            </InputAdornment>
+                        ),
+                    }}
+                    type="number"
+                    name='ssn'
+                    required
+                />
+
                 <Container className="SecondTextField">
-                
-                <TextField
-                                    label="Contraseña"
-                                    variant="standard"
-                                    InputProps={{
-                                        startAdornment: (
-                                            <InputAdornment position="start">
-                                                <LockIcon />
-                                            </InputAdornment>
-                                        ),
-                                    }}
-                                    type="password"
-                                    name="password"
-                                    required
-                                />
+
+                    <TextField
+                        label="Contraseña"
+                        variant="standard"
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <LockIcon/>
+                                </InputAdornment>
+                            ),
+                        }}
+                        type="password"
+                        name="password"
+                        required
+                    />
                 </Container>
                 <Container fluid>
                     <Button type="submit" className="AccesButton">Iniciar Sesion</Button>
                 </Container>
             </Form>
-            <Link to="/reset-password/"><Button variant="link" className="link">Me olvidé la contraseña</Button></Link>
-            <Link to="/signin/"><Button variant="link" className="link">No tengo una cuenta</Button></Link>
-            
-            </Card>
+            <Link to="/reset-password/"><Button variant="link" className="link">Olvidé mi contraseña</Button></Link>
+            <Link to="/signin/"><Button variant="link" className="link">Crea una cuenta</Button></Link>
+
+
         </Container>
-        
+
     )
 };
 
