61e0c3214347023f0f1fb4222ea409b08f0ef6a7
Mateo Marchisone <mateo.marchisone@gmail.com>
2023-08-30 12:47:31 -0300

    Template data

--- financiation-react/src/pages/MainMenuPage.jsx
+++ financiation-react/src/pages/MainMenuPage.jsx
@@ -5,6 +5,7 @@ import Avatar from "@mui/material/Avatar";
 import React, {useContext} from "react";
 import AuthContext from "../context/AuthContext";
 import VisitCardMainMenu from "../components/VisitCardMainMenu";
+import PieChart from "../components/PieChart";
 
 export const MainMenuPage = () => {
 
@@ -39,8 +40,8 @@ export const MainMenuPage = () => {
                         </Container>
                     </Card>
                     <Card className="main-menu-card last-requests-card" id="left-card">
-                        <h1 className="name-title">Consultas resueltas en la ultima visita</h1>
-                        <h1 className="name-title">53</h1>
+                        <h2 className="name-title">Consultas resueltas en la ultima visita:</h2>
+                        <h1 className="name-title fw-bold">53</h1>
                     </Card>
 
                 </Col>
@@ -67,32 +68,32 @@ export const MainMenuPage = () => {
                 </Col>
 
                 <Col lg={4}>
-                    <Card className="main-menu-card" id="right-card">
-                        <Container fluid className="avatar_container">
-                            <Avatar src={myUser?.profile_picture} className="profile_picture_avatar"/>
-                        </Container>
+                    <Card className="main-menu-card group-card-main-menu" id="right-card">
+                        <h1 className="name-title">Grupo 1</h1>
+                        <Row>
+                            <Col lg={6}>
+                                <h5>Mateo Marchisone</h5>
+                            </Col>
+                            <Col lg={6}>
+                                <p>Coordinador</p>
+                            </Col>
+                        </Row>
 
-                        <h1 className="name-title">{myUser?.first_name} {myUser?.last_name}</h1>
 
-                        <Container>
-                            <Container>
-                                <h5 className="property-title">Estado:</h5>
-                                <p>{myUser?.user_status}</p>
-
-                            </Container>
-                            <Container>
-                                <h5 className="property-title">Rol:</h5>
-                                <p>{myUser?.role}</p>
-                            </Container>
-                            <Container>
-                                <h5 className="property-title">CUIL:</h5>
-                                <p>{myUser?.ssn}</p>
-                            </Container>
-                        </Container>
                     </Card>
-                    <Card className="main-menu-card" id="right-card">
-                        <h1 className="name-title">Consultas resueltas en la ultima visita</h1>
-                        <h1 className="name-title">53</h1>
+                    <Card className="main-menu-card report-card-main-menu" id="right-card">
+                        <h1 className="name-title">Reportes</h1>
+                        <Container fluid>
+                            <PieChart chartData={{
+                            labels: ["Pedro", "Maria", "Juan"],
+                            datasets: [{
+                                label: "",
+                                data: [30,30,40],
+                                backgroundColor: ["red","green","blue"]
+                            }]
+                        }}/>
+                        </Container>
+
                     </Card>
 
                 </Col>
