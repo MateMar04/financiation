8dee1488947777fb94c4bd6a9595fcdd550f9b0d
Mateo Marchisone <mateo.marchisone@gmail.com>
2023-08-31 12:45:25 -0300

    Authentication for put

--- financiation-react/src/components/ProfilePicture.jsx
+++ financiation-react/src/components/ProfilePicture.jsx
@@ -1,7 +1,5 @@
-import React, { useContext,useState } from "react";
-import { Container } from "react-bootstrap";
+import React, {useContext, useState} from "react";
 import '../assets/styles/ProfilePicture.css';
-import {SucceedModal} from "../components/SucceedModal"
 import AuthContext from "../context/AuthContext";
 
 
@@ -10,26 +8,35 @@ import AuthContext from "../context/AuthContext";
 //     const toggleModalsucceed = () => setShowsuccese(!showsuccess);
 //     let {authTokens} = useContext(AuthContext)
 
-export const ProfilePicture= () =>  {
+export const ProfilePicture = () => {
     const [file, setFile] = useState(null);
     const [message, setMessage] = useState('');
-    
+    const {authTokens} = useContext(AuthContext)
+
     const handleFileChange = (e) => {
         setFile(e.target.files[0]);
     };
-    
+
+    let headers = {
+        "Content-Type": "application/json",
+        "Authorization": "JWT " + String(authTokens.access),
+        "Accept": "application/json"
+    }
+
     const handleSubmit = async (e) => {
         e.preventDefault();
-    
+
         try {
+            console.log(file)
             const response = await fetch('/api/update-profile-picture', {
                 method: 'PUT',
+                headers: headers,
                 body: file
             });
-    
+
             if (!response.ok) {
                 throw new Error('Network response was not ok');
-             }
+            }
 
             const data = await response.json();
             console.log('Response from server:', data);
@@ -38,12 +45,12 @@ export const ProfilePicture= () =>  {
         }
     };
 
-    
+
     return (
         <div>
             <h2>Update Profile Picture</h2>
             <form onSubmit={handleSubmit}>
-                <input name="imageFile" type="file" accept="image/*" onChange={handleFileChange} />
+                <input name="imageFile" type="file" accept="image/*" onChange={handleFileChange}/>
                 <button type="submit">Update</button>
             </form>
             <p>{message}</p>
@@ -51,13 +58,13 @@ export const ProfilePicture= () =>  {
     );
 };
 //     return (
-        
+
 //             <Container className="profile-picture">
 //                 <img src={profileImg} alt={username} className="responsive-image" />
 //                 <input type="file" />
 //                 <button type="submit" onClick={putpicture}>Anadir </button>
 //             </Container>
-        
+
 //     );
 // }
 
