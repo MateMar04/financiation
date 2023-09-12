d0208c89d0a40781229b3e96c5f04ece29e6390b
LorenzoGalaverna <lorenzo.galaverna1@gmail.com>
2023-09-12 12:19:32 -0300

    ready for pullrequest

--- financiationDjango/financiationAPI/urls.py
+++ financiationDjango/financiationAPI/urls.py
@@ -89,7 +89,7 @@ urlpatterns = [
 
     #EditarProfilePic
     path('update-profile-picture', views.ProfilePictureView.as_view(), name='ProfilePicture'),
-,
+    
     #mayors
     path('mayors/<int:id>', views.getMayorById, name='mayor'),
     path('mayors/delete/<int:id>', views.deleteMayorById, name='delete_mayor'),
