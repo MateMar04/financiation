181b1a8a6618826c8693e2eb014db8dcff3c3b98
Julian Rosas <julymaxrosas@gmail.com>
2023-09-12 09:55:12 -0300

    Palette done!

--- financiation-react/src/context/ReportsContext.jsx
+++ financiation-react/src/context/ReportsContext.jsx
@@ -251,7 +251,7 @@ export const ReportsProvider = ({children}) => {
         }
     }
 
-    const colors = ['#ffa43a', '#ffbf75', '#759eff', '#75c7ff', '#ff5252', '#50723C', '#cbdcdf', '#B87D4B','#c7ede8','#3b3024']
+    const colors = ['#ffa43a', '#ffbf75', '#759eff', '#75c7ff', '#ff5252', '#4fc468', '#b66be8', '#B87D4B','#c7ede8']
 
     const [advisorsData, setAdvisorsData] = useState({
         labels: [],
