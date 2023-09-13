6ca4a1e5f1e8b2d6be9074a6a4c53ec3122cf628
Mateo Marchisone <mateo.marchisone@gmail.com>
2023-09-13 16:54:26 -0300

    Added services

--- financiation-react/src/services/VisitServices.js
+++ financiation-react/src/services/VisitServices.js
@@ -8,3 +8,25 @@ export const getVisits = async (tokens) => {
     let data = await response.json()
     return data
 };
+
+export const getLatestVisits = async (tokens) => {
+    let headers = {
+        "Content-Type": "application/json",
+        "Authorization": "JWT " + String(tokens),
+        "Accept": "application/json"
+    }
+    let response = await fetch('/api/visits/latest', {headers: headers})
+    let data = await response.json()
+    return data
+};
+
+export const getLatestVisitRequests = async (tokens) => {
+    let headers = {
+        "Content-Type": "application/json",
+        "Authorization": "JWT " + String(tokens),
+        "Accept": "application/json"
+    }
+    let response = await fetch('/api/visits/latest/requests', {headers: headers})
+    let data = await response.json()
+    return data
+};
