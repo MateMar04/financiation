e3888a88e13fddec642b6b68638c9b5bf3154caf
LautaroVelez <lautivelez28@gmail.com>
2023-09-14 13:19:49 -0300

    BtnFlotante agregado

--- financiation-react/src/pages/GroupsPage.jsx
+++ financiation-react/src/pages/GroupsPage.jsx
@@ -1,11 +1,17 @@
-import {useContext, useEffect, useState} from "react";
+import { useContext, useEffect, useState } from "react";
 import AuthContext from "../context/AuthContext";
 import GroupCard from "../components/GroupCard";
-import {Container} from "react-bootstrap";
-import {getGroups} from "../services/GroupServices"
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { getGroups } from "../services/GroupServices"
+
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import NavigationIcon from '@mui/icons-material/Navigation';
 
 export const GroupsPage = () => {
-    let {authTokens} = useContext(AuthContext)
+    let { authTokens } = useContext(AuthContext)
     let [groups, setGroups] = useState([])
 
     useEffect(() => {
@@ -15,9 +21,16 @@ export const GroupsPage = () => {
     return (
         <div>
             <Container>
+                <Fab
+                    size='medium'
+                    className="BtnFlotante"
+                    color="primary">
+                    <AddIcon />
+                </Fab>
+
                 {groups?.map((group) => (
                     <Container>
-                        <GroupCard group={group}/>
+                        <GroupCard group={group} />
                     </Container>
                 ))}
             </Container>
