408c37887af4bd848adb7c5e25fdeef5f8bde966
LautaroVelez <lautivelez28@gmail.com>
2023-09-14 16:05:52 -0300

    reports done

--- financiation-react/src/pages/ReportsPage.jsx
+++ financiation-react/src/pages/ReportsPage.jsx
@@ -63,29 +63,29 @@ export const ReportsPage = () => {
                         <ReportFilterCard title="Motivos" items={Object.values(faqs)} tokens={authTokens.access}/>
                     </Col>
                 </Row>
-                <Row className='justify-content-center'>
+                <Row className='justify-content-center text-center'>
                     <Col lg={2}>
-                        <Button  onClick={() => buttonClick()} className='BtnGenera'>Generar</Button>
+                        <Button  onClick={() => buttonClick()} className='BtnGenera'>Generar gráfico</Button>
                     </Col>
                 </Row>
             </Container>
 
             
 
-            <Container className='container1'>
-                <Row>
-                    <Col lg={6} className='chart-column'>
+            <Container className='container1 container-white'>
+                <Row className='justify-content-center text-center'>
+                    <Col lg={6} >
                         <ReportChartCard title="Visitas" chart={<BarChart chartData={visitsData}/>}/>
                     </Col>
-                    <Col lg={6} className='chart-column'>
+                    <Col lg={6} >
                         <ReportChartCard title="Organismos" chart={<BarChart chartData={ministryDepsData}/>}/>
                     </Col>
                 </Row>
-                <Row>
-                    <Col lg={6} className='chart-column'>
+                <Row className='justify-content-center text-center'>
+                    <Col lg={6} >
                         <ReportChartCard title="Motivos" chart={<PieChart chartData={faqsData}/>}/>
                     </Col>
-                    <Col lg={6} className='chart-column'>
+                    <Col lg={6} >
                         <ReportChartCard title="Asesores" chart={<PolarAreaChart chartData={advisorsData}/>}/>
                     </Col>
                 </Row>
