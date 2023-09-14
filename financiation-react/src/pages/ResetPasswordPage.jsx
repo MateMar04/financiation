31794e1f1a2e473d28084ce697017b7eed794fc2
LautaroVelez <lautivelez28@gmail.com>
2023-09-14 10:55:40 -0300

    modal reset password page

--- financiation-react/src/pages/ResetPasswordPage.jsx
+++ financiation-react/src/pages/ResetPasswordPage.jsx
@@ -1,21 +1,35 @@
-import React from "react";
-import {Button, Card, Col, Container, FloatingLabel, Form, Modal, Row} from "react-bootstrap";
+
+import React, { useState } from "react";
+import { Button, Col, Container, FloatingLabel, Form, Row } from "react-bootstrap";
 import Logo from "../assets/images/PRUEBA.PNG";
-import Check from "../assets/images/checked.gif";
-import {Link, useNavigate} from "react-router-dom";
+
+import { Link, useNavigate } from "react-router-dom";
 import '../assets/styles/ResetPassword.css'
 import TextField from '@mui/material/TextField';
 import InputAdornment from '@mui/material/InputAdornment';
-import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
-import VpnKeyOutlinedIcon from '@mui/icons-material/VpnKeyOutlined';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import { Modal } from 'antd';
 
 const ResetPasswordPage = () => {
 
     let history = useNavigate()
     const [show, setShow] = React.useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+
+    const showModal = () => {
+       
+        setIsModalOpen(true);
+    };
+
+    const handleOk = () => {
+        setIsModalOpen(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
 
     let resetPasswordEmail = async (e) => {
         e.preventDefault()
@@ -41,45 +55,45 @@ const ResetPasswordPage = () => {
     return (
 
         <Container fluid className="fondo">
-             <Container className="ContainerResetPassword">
-
-<Row className={'justify-content-center text-center'}>
-    <img src={Logo} className="ImgLogoResetPassword" alt="Logo del ministerio de finanzas"/>
-</Row>
-<Row className={'justify-content-center text-center'}>
-                <h3 className={'h3ResetPasswordPage'}>Reestablece tu contraseña</h3>
-            </Row>
-            <Row className={'justify-content-center text-center'}>
-                <p className={'pResetPassword'}>No recuerdas tu contraseña? Introduce tu email para cambiarla!</p>
-
-            </Row>
-            <Form>
+            <Container className="ContainerResetPassword">
 
                 <Row className={'justify-content-center text-center'}>
-                    <Form.Group>
-                        <TextField
-                            label="Email"
-                            variant="outlined"
-                            InputProps={{
-                                startAdornment: (
-                                    <InputAdornment position="start"> <MailOutlineIcon/> </InputAdornment>),
-                                sx: {borderRadius: 6, borderColor: '#f4f4f4'}
-                            }}
-                            type="text"
-                            name='ssn'
-                            required
-                            className={'InputResetPassword'}
-                        />
-                    </Form.Group>
-
-                    
+                    <img src={Logo} className="ImgLogoResetPassword" alt="Logo del ministerio de finanzas" />
                 </Row>
-                
                 <Row className={'justify-content-center text-center'}>
-                    <Button type="submit" className="BtnEnviarResetPassword">Enviar</Button>
+                    <h3 className={'h3ResetPasswordPage'}>Reestablece tu contraseña</h3>
                 </Row>
-            </Form>
-        </Container>
+                <Row className={'justify-content-center text-center'}>
+                    <p className={'pResetPassword'}>¿No recuerdas tu contraseña? Introduce tu correo electrónico para cambiarla.</p>
+
+                </Row>
+                <Form>
+                    <Row className={'justify-content-center text-center'}>
+                        <Form.Group>
+                            <TextField
+                                label="Correo Electrónico"
+                                variant="outlined"
+                                InputProps={{
+                                    startAdornment: (
+                                        <InputAdornment position="start"> <MailOutlineIcon /> </InputAdornment>),
+                                    sx: { borderRadius: 6, borderColor: '#f4f4f4' }
+                                }}
+                                type="text"
+                                required
+                                className={'InputResetPassword'}
+                            />
+                        </Form.Group>
+                    </Row>
+
+                    <Row className={'justify-content-center text-center'}>
+                        <Button type="submit" className="BtnEnviarResetPassword" onClick={showModal}>Enviar</Button>
+                    </Row>
+                </Form>
+                <Modal title="Activación de cuenta" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+                  <p>Se ha enviado el mail</p>
+                </Modal>
+            </Container>
+
         </Container>
     );
 }
