70af2e035c38be87789c95bebe3fd8883e0b9dc4
LautaroVelez <lautivelez28@gmail.com>
2023-09-14 14:47:34 -0300

    groups page button added

--- financiation-react/src/pages/GroupsPage.jsx
+++ financiation-react/src/pages/GroupsPage.jsx
@@ -3,12 +3,11 @@ import AuthContext from "../context/AuthContext";
 import GroupCard from "../components/GroupCard";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import { getGroups } from "../services/GroupServices"
-
+import '../assets/styles/GroupsPage.css'
 import Fab from '@mui/material/Fab';
 import AddIcon from '@mui/icons-material/Add';
-import EditIcon from '@mui/icons-material/Edit';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import NavigationIcon from '@mui/icons-material/Navigation';
+import { Link } from "react-router-dom";
+
 
 export const GroupsPage = () => {
     let { authTokens } = useContext(AuthContext)
@@ -21,13 +20,17 @@ export const GroupsPage = () => {
     return (
         <div>
             <Container>
-                <Fab
-                    size='medium'
-                    className="BtnFlotante"
-                    color="primary">
-                    <AddIcon />
-                </Fab>
-
+                <Link to='/groups/add'>
+                    <Fab
+                        variant="extended"
+                        size='medium'
+                        className="BtnFlotante"
+                        color="primary"
+                        >
+                        <AddIcon />
+                        Nuevo Grupo
+                    </Fab>
+                    </Link>
                 {groups?.map((group) => (
                     <Container>
                         <GroupCard group={group} />
