38c2e97b0c25d5ab0165b5a871441c8117f74e94
Mateo Marchisone <mateo.marchisone@gmail.com>
2023-09-14 14:45:04 -0300

    Bold

--- financiation-react/src/components/PersonRowMainMenu.jsx
+++ financiation-react/src/components/PersonRowMainMenu.jsx
@@ -4,7 +4,7 @@ export const PersonRowMainMenu = ({role, first_name, last_name}) => {
     return (
         <Container fluid>
             <Row>
-                <Col lg={6}>{role}:</Col>
+                <Col lg={6} className="fw-bold">{role}:</Col>
                 <Col lg={6}>{first_name} {first_name}</Col>
             </Row>
         </Container>
