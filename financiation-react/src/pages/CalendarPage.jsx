56aa9c7eb8dda14f3080c8f2d19be947eed2942b
LautaroVelez <lautivelez28@gmail.com>
2023-09-13 22:23:34 -0300

    calendar added modal and visit day (front)

--- financiation-react/src/pages/CalendarPage.jsx
+++ financiation-react/src/pages/CalendarPage.jsx
@@ -2,91 +2,86 @@ import React, {useContext, useEffect, useState} from "react";
 import {Container, Row, Col} from 'react-bootstrap';
 import '../assets/styles/CalendarPage.css'
 import locale from 'antd/locale/es_ES';
-import dayjs from 'dayjs';
+import Dayjs from 'dayjs';
 import 'dayjs/locale/es';
 import {ConfigProvider} from 'antd';
 import {Badge, Calendar} from 'antd';
 import {getVisits} from "../services/VisitServices";
 import AuthContext from "../context/AuthContext";
+import {Modal} from 'antd';
+import {useNavigate} from 'react-router-dom';
+
 
 const getListData = (value: Dayjs) => {
     let listData;
     switch (value.date()) {
-        case 8:
+        case 11:
             listData = [
-                {type: 'warning', content: 'This is warning event.'},
-                {type: 'success', content: 'This is usual event.'},
-            ];
-            break;
-        case 10:
-            listData = [
-                {type: 'warning', content: 'This is warning event.'},
-                {type: 'success', content: 'This is usual event.'},
-                {type: 'error', content: 'This is error event.'},
+                {type: 'success', content: 'Visita a EL DURAZNO'},
             ];
             break;
         case 15:
             listData = [
-                {type: 'warning', content: 'This is warning event'},
-                {type: 'success', content: 'This is very long usual event......'},
-                {type: 'error', content: 'This is error event 1.'},
-                {type: 'error', content: 'This is error event 2.'},
-                {type: 'error', content: 'This is error event 3.'},
-                {type: 'error', content: 'This is error event 4.'},
+                {type: 'error', content: 'Visita cancelada'},
             ];
             break;
         default:
     }
     return listData || [];
 };
+
 export const CalendarPage = () => {
-     let [visits, setVisits] = useState([])
-    let {authTokens, myUser} = useContext(AuthContext)
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedDate, setSelectedDate] = useState(null);
+    const [visits, setVisits] = useState([]);
+    const {authTokens} = useContext(AuthContext);
+    const navigate = useNavigate();
 
-     useEffect(() => {
-
-        getVisits(authTokens.access).then(data => setVisits(data))
-
-
-    }, [])
-    const monthCellRender = (value) => {
-        const num = getMonthData(value);
-        return num ? (
-            <div className="notes-month">
-                <section>{num}</section>
-                <span>Backlog number</span>
-            </div>
-        ) : null;
-    };
-    const dateCellRender = (value) => {
-        const listData = getListData(value);
-        return (
-            <ul className="events">
-                {listData.map((item) => (
-                    <li key={item.content}>
-                        <Badge status={item.type} text={item.content}/>
-                    </li>
-                ))}
-            </ul>
-        );
-    };
-    const cellRender = (current, info) => {
-        if (info.type === 'date') return dateCellRender(current);
-        if (info.type === 'month') return monthCellRender(current);
-        return info.originNode;
+    const showModal = (date) => {
+        setSelectedDate(date);
+        setIsModalOpen(true);
     };
 
-    const getMonthData = (value: Dayjs) => {
-        if (value.month() === 8) {
-            return 1394;
-        }
+    const handleOk = () => {
+        setIsModalOpen(false);
+        navigate('/visit/add');
     };
+
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
+
+    useEffect(() => {
+        getVisits(authTokens.access).then(data => setVisits(data));
+    }, []);
+
+
+    function isWeekend(date) {
+        const dayOfWeek = new Date(date).getDay();
+        return dayOfWeek === 0 || dayOfWeek === 6;
+    }
+
     return (
         <Container fluid>
             <ConfigProvider locale={locale}>
-                <Calendar className={'CalendarCalendarPage'} cellRender={cellRender}/>
+                <Calendar className={'CalendarCalendarPage'} onSelect={showModal}
+                          disabledDate={(date) => isWeekend(date)}
+                          dateCellRender={(date) => {
+                              if (new Date(date).getDate() === new Date().getDate()){
+                                  return <h5>Visita</h5>;}
+                          }}
 
-            </ConfigProvider>;
+                          monthCellRender={(date) =>{
+                               if (new Date(date).getMonth() === new Date().getMonth()){
+                                  return <h5>visitas: 2</h5>;}
+                          }}
+
+                />
+            </ConfigProvider>
+
+            <Modal title="Agregar Visita" visible={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+                {selectedDate && <p>¿Quiere agregar una visita para el día {selectedDate.format('DD/MM/YYYY')}?</p>}
+            </Modal>
         </Container>
 
     );
