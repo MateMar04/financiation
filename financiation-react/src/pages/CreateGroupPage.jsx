5536f3702febef79a07caedc6d232816d76d56db
tadeofunes <71388800+tadeofunes@users.noreply.github.com>
2023-11-02 11:02:17 -0300

    Modifing button to the right site

--- financiation-react/src/pages/CreateGroupPage.jsx
+++ financiation-react/src/pages/CreateGroupPage.jsx
@@ -10,6 +10,7 @@ import { FailedModal } from "../components/FailedModal"
 import { getUsers } from "../services/UserServices";
 import GroupsIcon from '@mui/icons-material/Groups';
 import { UserCard } from "../components/UserCard";
+import FlotantButton from "../components/FlotantButton";
 
 export const CreateGroupPage = () => {
     let { authTokens } = useContext(AuthContext)
@@ -96,7 +97,8 @@ export const CreateGroupPage = () => {
                 <Row className='text-center'>
                     <Col>
                         <Form.Group>
-                            <Button type="submit" className={'BtnCrearGrupo'}>Crear Grupo</Button>
+
+                            <FlotantButton type="submit" className={'BtnCrearGrupo'}name='Crear Grupo'  />
 
                         </Form.Group>
                     </Col>
