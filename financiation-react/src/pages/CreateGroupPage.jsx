02756d42065c1d934612015e81e2c72dda9bdd62
LorenzoGalaverna <lorenzo.galaverna1@gmail.com>
2023-11-05 18:03:01 -0300

    search input now working in real time

--- financiation-react/src/pages/CreateGroupPage.jsx
+++ financiation-react/src/pages/CreateGroupPage.jsx
@@ -12,97 +12,110 @@ import GroupsIcon from '@mui/icons-material/Groups';
 import { UserCard } from "../components/UserCard";
 
 export const CreateGroupPage = () => {
-    let { authTokens } = useContext(AuthContext)
+    let { authTokens } = useContext(AuthContext);
     const [showfail, setShowfailture] = useState(false);
     const [showsuccess, setShowsuccese] = useState(false);
-    let [users, setUsers] = useState([])
+    const [users, setUsers] = useState([]);
+    const [searchQuery, setSearchQuery] = useState("");
+    const [filteredUsers, setFilteredUsers] = useState([]);
 
     const toggleModalsucceed = () => setShowsuccese(!showsuccess);
     const toggleModalfailed = () => setShowfailture(!showfail);
 
     useEffect(() => {
-        getUsers(authTokens.access).then(data => setUsers(data))
-    }, [])
+        getUsers(authTokens.access).then(data => {
+            console.log("User Data:", data);
+            setUsers(data);
+            setFilteredUsers(data);
+        });
+    }, [authTokens.access]);
+
+    const handleSearch = (e) => {
+        const query = e.target.value.toLowerCase();
+        setSearchQuery(query);
+    
+        const filtered = users.filter(user => user.first_name && user.first_name.toLowerCase().includes(query));
+        setFilteredUsers(filtered);
+    };    
 
     let postGroup = async (e) => {
-        e.preventDefault()
-        let response = await fetch('/api/groups', {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json',
-                "Authorization": "JWT " + String(authTokens.access),
-                "Accept": "application/json"
-            },
-            body: JSON.stringify({ "name": e.target.name.value })
-        })
-        if (response.status === 200) {
-            toggleModalsucceed();
-            await postGroup()
-        } else if (response.status === 500) {
-            toggleModalfailed();
-            await postGroup()
-        } else if (response.status === 401) {
-            toggleModalfailed();
-            await postGroup()
-        } else if (response.status === 400) {
-            toggleModalfailed();
-            await postGroup()
+        if (e && e.target) {
+            e.preventDefault();
+            let response = await fetch('/api/groups', {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                    "Authorization": "JWT " + String(authTokens.access),
+                    "Accept": "application/json"
+                },
+                body: JSON.stringify({ "name": e.target.name.value })
+            })
+            if (response.status === 200) {
+                toggleModalsucceed();
+                await postGroup();
+            } else if (response.status === 500) {
+                toggleModalfailed();
+                await postGroup();
+            } else if (response.status === 401) {
+                toggleModalfailed();
+                await postGroup();
+            } else if (response.status === 400) {
+                toggleModalfailed();
+                await postGroup();
+            }
         }
     }
 
     return (
-
-            <Container fluid>
-                <SucceedModal message="el coordinador" onClose={() => toggleModalsucceed()} show={showsuccess}/>
-                <FailedModal message="el coordinador" onClose={() => toggleModalfailed()} show={showfail}/>
+        <Container fluid>
+            <SucceedModal message="el coordinador" onClose={() => toggleModalsucceed()} show={showsuccess} />
+            <FailedModal message="el coordinador" onClose={() => toggleModalfailed()} show={showfail} />
 
             <Form onSubmit={postGroup}>
                 <Container className="separation font text-center justify-content-center">
                     <Row className='justify-content-center text-center'>
-                    <Col md={10}>
-                        <TextField
-                            fullWidth
-                            id="SearchVisit"
-                            variant="outlined"
-                            label='Buscar'
-                            className="SearchVisit"
-                            InputProps={{
-                                sx: { borderRadius: 5, color: "black" },
-                                endAdornment: (
-                                    <IconButton>
-                                        <SearchIcon />
-                                    </IconButton>
-                                ),
-                            }}
-                        />
-                    </Col>
+                        <Col md={10}>
+                            <TextField
+                                fullWidth
+                                id="SearchVisit"
+                                variant="outlined"
+                                label='Buscar'
+                                className="SearchVisit"
+                                InputProps={{
+                                    sx: { borderRadius: 5, color: "black" },
+                                    endAdornment: (
+                                        <IconButton>
+                                            <SearchIcon />
+                                        </IconButton>
+                                    ),
+                                }}
+                                value={searchQuery}
+                                onChange={handleSearch}
+                            />
+                        </Col>
 
                         <Col md={2} xs={1} lg={1}>
-                            <IconButton href="/groups/" sx={{ width: 56, height: 56 }}
-                                className={'GroupsIcon'}><GroupsIcon/></IconButton>
+                            <IconButton href="/groups/" sx={{ width: 56, height: 56 }} className={'GroupsIcon'}><GroupsIcon /></IconButton>
                         </Col>
                     </Row>
                 </Container>
 
                 <Container className="justify-content-center">
-                    {users?.map((user) => (
-                        <Container className="containerUserCard justify-content-center text-center">
+                    {filteredUsers.map((user) => (
+                        <Container key={user.id} className="containerUserCard justify-content-center text-center">
                             <UserCard user={user} />
                         </Container>
                     ))}
                 </Container>
 
-
                 <Row className='text-center'>
                     <Col>
                         <Form.Group>
                             <Button type="submit" className={'BtnCrearGrupo'}>Crear Grupo</Button>
-
                         </Form.Group>
                     </Col>
                 </Row>
-
-            </Form >
-        </Container >
+            </Form>
+        </Container>
     )
 }
