6f981cf99426ad7d2135a703cbcfa268ec23675f
LautaroVelez <lautivelez28@gmail.com>
2023-11-05 21:43:06 -0300

    create coordinator cambiado modal, falta el select

--- financiation-react/src/components/AdvisorCreateModal.jsx
+++ financiation-react/src/components/AdvisorCreateModal.jsx
@@ -1,17 +1,11 @@
 import '../assets/styles/RowWithCheck.css'
-import {Col, Row } from "react-bootstrap";
-import { Form } from "react-bootstrap";
-import React, { useContext, useState, useEffect } from "react";
+import {Form} from "react-bootstrap";
+import React, {useContext, useState, useEffect} from "react";
 import AuthContext from "../context/AuthContext";
-import TextField from '@mui/material/TextField';
-import InputAdornment from '@mui/material/InputAdornment';
-import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
-import { Container, Card } from 'react-bootstrap';
 import '../assets/styles/AddMayorPage.css';
 import FailedModal from "./FailedModal";
 import SucceedModal from "./SucceedModal";
 import {getUsers} from "../services/UserServices";
-import Box from '@mui/material/Box';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import InputLabel from '@mui/material/InputLabel';
@@ -20,7 +14,7 @@ import {message, Button, Modal} from 'antd';
 
 export const AdvisorCreateModal = (props) => {
 
-    let { authTokens } = useContext(AuthContext)
+    let {authTokens} = useContext(AuthContext)
     const [showfail, setShowfailture] = useState(false);
     const [showsuccess, setShowsuccese] = useState(false);
     const [users, setUsers] = React.useState();
@@ -32,7 +26,7 @@ export const AdvisorCreateModal = (props) => {
     }, []);
 
     let postAdvisor = async () => {
-        if (selectedUserId) { 
+        if (selectedUserId) {
             let response = await fetch('/api/advisors', {
                 method: "POST",
                 headers: {
@@ -46,65 +40,60 @@ export const AdvisorCreateModal = (props) => {
                 })
             })
             if (response.status === 200) {
-                toggleModalsucceed();
+                // toggleModalsucceed();
+                message.success('Se creó el asesor correctamente');
                 props.setUpdateFlag((prevFlag) => !prevFlag);
                 props.onClose();
             } else {
-                 toggleModalfailed();
-
+                // toggleModalfailed();
+                message.error('No se pudo crear el asesor');
                 props.onClose();
             }
         } else {
-           toggleModalfailed()
-
+            // toggleModalfailed()
+            message.error('No se pudo crear el asesor');
             props.onClose();
         }
     }
 
     return (
-         <Modal
-                open={props.show}
-                title="Agregar Asesor"
-                onCancel={props.onClose}
-                footer={[
-                    <Button key="back" onClick={props.onClose}>
-                        Cancelar
-                    </Button>,
-                    <Button
-                         onClick={postAdvisor}
-                        type="primary"
+        <Modal
+            open={props.show}
+            title="Agregar Asesor"
+            onCancel={props.onClose}
+            footer={[
+                <Button key="back" onClick={props.onClose}>
+                    Cancelar
+                </Button>,
+                <Button
+                    onClick={postAdvisor}
+                    type="primary"
+                >
+                    Agregar
+                </Button>
+            ]}
+        >
+            <SucceedModal onClose={() => toggleModalsucceed()} message="la visita" show={showsuccess}/>
+            <FailedModal onClose={() => toggleModalfailed()} message="la visita" show={showfail}/>
+
+            <Form>
+                <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">Nombre de la persona</InputLabel>
+                    <Select
+                        label="Nombre de la persona"
+                        value={selectedUserId}
+                        onChange={(event) => {
+                            console.log('Selected User ID:', event.target.value);
+                            setSelectedUserId(event.target.value);
+                        }}
                     >
-                        Agregar
-                    </Button>
-                ]}
-            >
-            <SucceedModal onClose={() => toggleModalsucceed()} message="la visita" show={showsuccess} />
-            <FailedModal onClose={() => toggleModalfailed()} message="la visita" show={showfail} />
-
-
-                <Form>
-                        <FormControl fullWidth>
-                            <InputLabel id="demo-simple-select-label">Nombre de la persona</InputLabel>
-                            <Select
-                                label="Nombre de la persona"
-                                value={selectedUserId} 
-                                onChange={(event) => {
-                                    console.log('Selected User ID:', event.target.value);
-                                    setSelectedUserId(event.target.value);
-                                }}
-                            >
-                                {users?.map((user) => (
-                                    <MenuItem key={user.id} value={user.id}>{user.first_name} {user.last_name}</MenuItem>
-                                ))}
-                            </Select>
-                        </FormControl>
-
-
-                </Form>
-
+                        {users?.map((user) => (
+                            <MenuItem key={user.id} value={user.id}>{user.first_name} {user.last_name}</MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+            </Form>
         </Modal>
-
-
     )
 }
 
