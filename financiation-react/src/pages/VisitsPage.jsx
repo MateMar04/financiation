aab0435ac717531c85b51dd2c9330e4deb67f4ea
LautaroVelez <lautivelez28@gmail.com>
2023-11-07 08:49:18 -0300

    visit page added

---
+++ financiation-react/src/pages/VisitsPage.jsx
@@ -0,0 +1,89 @@
+import {useContext, useEffect, useState} from "react";
+import AuthContext from "../context/AuthContext";
+import {getVisits} from "../services/VisitServices";
+import {TextField} from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
+import IconButton from '@mui/material/IconButton';
+import {Col, Container, Row} from "react-bootstrap";
+import FlotantButton from "../components/FlotantButton";
+import {Link} from "react-router-dom";
+import {Pagination} from "antd";
+import {VisitCard} from "../components/VisitCard";
+import "../assets/styles/VisitPage.css"
+
+
+export const VisitsPage = () => {
+
+    useEffect(() => {
+        getVisits(authTokens.access).then(r => setVisits(r))
+    }, [])
+
+    let {authTokens} = useContext(AuthContext)
+    let [visits, setVisits] = useState([])
+
+    const [currentPage, setCurrentPage] = useState(1)
+    const [itemsPerPage, setItemsPerPage] = useState(10)
+    const totalItems = visits.length;
+
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+
+    const currentData = visits.slice(startIndex, endIndex)
+
+    const handlePageChange = (page) => {
+        setCurrentPage(page);
+    };
+
+    const handleSizeChange = (number) => {
+
+    };
+
+
+    return (
+
+
+        <Container>
+            <Container>
+                <Link to='/visit/add/'>
+                    <FlotantButton name={'Nueva Visita'}/>
+                </Link>
+            </Container>
+            <Row className='justify-content-center text-center'>
+                <Col md={10}>
+                    <TextField
+                        fullWidth
+                        id="SearchVisit"
+                        variant="outlined"
+                        label='Buscar'
+                        className="SearchVisit"
+                        InputProps={{
+                            sx: {borderRadius: 5, color: "black"},
+                            endAdornment: (
+                                <IconButton>
+                                    <SearchIcon/>
+                                </IconButton>
+                            ),
+                        }}
+                    />
+                </Col>
+            </Row>
+
+            <Container>
+                <Row className='justify-content-center text-center'>
+                    <Col md={8}>
+                        {currentData?.map(visit => (<VisitCard visit={visit}/>))}
+                    </Col>
+
+                </Row>
+                <Pagination className='justify-content-center text-center visit-pagination-menu'
+                            defaultCurrent={1}
+                            onChange={handlePageChange}
+                            total={totalItems}
+                            defaultPageSize={10}
+                            showSizeChanger={false}
+                            current={currentPage}/>
+
+            </Container>
+        </Container>
+    )
+}
