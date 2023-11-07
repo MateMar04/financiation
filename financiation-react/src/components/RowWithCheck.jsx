f5cd1f673d8c640f0a9c936c023812eb6b4dc7a3
LorenzoGalaverna <lorenzo.galaverna1@gmail.com>
2023-11-07 10:20:33 -0300

    new buttons

--- financiation-react/src/components/RowWithCheck.jsx
+++ financiation-react/src/components/RowWithCheck.jsx
@@ -15,35 +15,15 @@ export const RowWithCheck = ({ item }) => {
   };
 
   return (
-    <Row key={item.id} className="labelfondo" onClick={handleRowClick}>
+    <Row key={item.id} className="labelfondo" >
       <Col className='row-label'>
         <Form.Label>{item.name}</Form.Label>
       </Col>
       <Col className='row-check'>
-        <Form.Check id={item.id} value={item.id} checked={isChecked}></Form.Check>
+        <Form.Check id={item.id} value={item.id} checked={isChecked} onClick={handleRowClick} className="custom-checkbox" ></Form.Check>
       </Col>
     </Row>
   );
 }
 
 export default RowWithCheck;
-
-// import {Col, Container, Form, Row} from "react-bootstrap";
-// import React from "react";
-// import '../assets/styles/RowWithCheck.css'
-// import "../assets/styles/ReportsPage.css"
-
-// export const RowWithCheck = ({item}) => {
-//     return (
-//         <Row key={item.id} className="labelfondo" >
-//             <Col className='row-label'>
-//                 <Form.Label>{item.name}</Form.Label>
-//             </Col>
-//             <Col className='row-check'>
-//                 <Form.Check id={item.id} value={item.id} defaultChecked={item.checked}></Form.Check>
-//             </Col>
-//         </Row>
-//     )
-// }
-
-// export default RowWithCheck
