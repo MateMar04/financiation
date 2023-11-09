40b3701f8e1baa69b9e8385b1ae6ddaa217455fb
LautaroVelez <lautivelez28@gmail.com>
2023-11-09 10:22:47 -0300

    mas lindo

--- financiation-react/src/components/PersonRowMainMenu.jsx
+++ financiation-react/src/components/PersonRowMainMenu.jsx
@@ -1,17 +1,21 @@
-import {Col, Container, Row} from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import "../assets/styles/MainMenuPage.css";
+import {Divider} from 'antd';
 
-export const PersonRowMainMenu = ({role, first_name, last_name}) => {
+export const PersonRowMainMenu = ({ role, first_name, last_name }) => {
     return (
-       
+        <>
             <Row className="containerGroupsNames">
-                <Col md={4}>
-                <b>{role}:</b>
+                <Col md={4} xs={5}>
+                    <b>{role}:</b>
                 </Col>
-                <Col md={8}>
-                <a>{first_name} {last_name}</a>
+                <Col md={8} xs={7}>
+                    <a>{first_name} {last_name}</a>
                 </Col>
             </Row>
-       
+            
+        </>
+
+
     )
 }
