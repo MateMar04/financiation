e4becece084cbc1fd53ab740e31b5cd9d6abb999
LautaroVelez <lautivelez28@gmail.com>
2023-11-21 11:19:13 -0300

    profile picture done

--- financiation-react/src/components/ProfilePicture.jsx
+++ financiation-react/src/components/ProfilePicture.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from "react";
 import { Button, Col, Row } from "react-bootstrap";
 import AuthContext from "../context/AuthContext";
+import "../assets/styles/ProfilePicture.css";
 
 export const ProfilePicture = ({ myUser, updateUserData }) => {
   const [file, setFile] = useState(null);
@@ -10,7 +11,7 @@ export const ProfilePicture = ({ myUser, updateUserData }) => {
   const handleFileChange = (e) => {
     setFile(e.target.files[0]);
     // Immediately update user data when a new picture is selected
-    
+
   };
 
   let headers = {
@@ -47,31 +48,28 @@ export const ProfilePicture = ({ myUser, updateUserData }) => {
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <Row className={"justify-content-center text-center"}>
-          <Col>
-            <label htmlFor="imageFile" className="SelectFileButton">
-              Cambiar Imagen
-            </label>
-            <input
-              id="imageFile"
-              name="imageFile"
-              type="file"
-              accept="image/*"
-              className="SelectFileInput"
-              onChange={handleFileChange}
-            />
-          </Col>
-        </Row>
-        <Row className={"justify-content-center text-center"}>
-          <Col>
-            <Button type="submit" className={"GuardarCambiosButton"}>
-              Guardar Cambios
-            </Button>
-          </Col>
-        </Row>
-      </form>
-      <p>{message}</p>
-    </div>
+            <form onSubmit={handleSubmit}>
+                <Row className={'justify-content-center text-center'}>
+                    <Col>
+                        <label htmlFor="imageFile" className="SelectFileButton">Cambiar Imagen</label>
+                        <input
+                            id="imageFile"
+                            name="imageFile"
+                            type="file"
+                            accept="image/*"
+                            className="SelectFileInput"
+                            onChange={handleFileChange}
+                        />
+                    </Col>
+                </Row>
+
+                <Row className={'justify-content-center text-center'}>
+                    <Col>
+                        <Button type="submit" className={'GuardarCambiosButton'}>Guardar Cambios</Button>
+                    </Col>
+                </Row>
+            </form>
+            <p>{message}</p>
+        </div>
   );
 };
