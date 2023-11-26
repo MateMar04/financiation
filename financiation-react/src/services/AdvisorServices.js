7b283b0d90d50354cd73cc407c780176de8559ed
Mateo Marchisone <mateo.marchisone@gmail.com>
2023-11-26 13:36:20 -0300

    top three endpoint

--- financiation-react/src/services/AdvisorServices.js
+++ financiation-react/src/services/AdvisorServices.js
@@ -19,3 +19,14 @@ export const getAdvisorUsers = async (tokens) => {
     let data = await response.json()
     return data
 }
+
+export const getTopThreeAdvisors = async (tokens) => {
+    let headers = {
+        "Content-Type": "application/json",
+        "Authorization": "JWT " + String(tokens),
+        "Accept": "application/json"
+    }
+    let response = await fetch('/api/top-three-advisors', {headers: headers})
+    let data = await response.json()
+    return data
+}
